51c4ac855ef9439ee46de0f995255da6e72bafab
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-17 23:34:11 -0500

    Update n_parenthesis.py

--- n_parenthesis.py
+++ n_parenthesis.py
@@ -1,16 +1,17 @@
-def generateParenthesis(n):
-    """
-    :type n: int
-    :rtype: List]
-    """
-    result = []
-    def backtrack(s, left, right):
-        if len(s) == 2 * n:
-            result.append(s)
-            return
-        if left < n:
-            backtrack(s + '(', left + 1, right)
-        if right < left:
-            backtrack(s + ')', left, right + 1)
-    backtrack('', 0, 0)
-    return result
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        """
+        :type n: int
+        :rtype: List]
+        """
+        result = []
+        def backtrack(s, left, right):
+            if len(s) == 2 * n:
+                result.append(s)
+                return
+            if left < n:
+                backtrack(s + '(', left + 1, right)
+            if right < left:
+                backtrack(s + ')', left, right + 1)
+        backtrack('', 0, 0)
+        return result
