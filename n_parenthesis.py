ee565a0ac74d73f1fd94b8dd9ad40141956f579c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-17 23:33:06 -0500

    Create n_parenthesis.py

---
+++ n_parenthesis.py
@@ -0,0 +1,16 @@
+def generateParenthesis(n):
+    """
+    :type n: int
+    :rtype: List]
+    """
+    result = []
+    def backtrack(s, left, right):
+        if len(s) == 2 * n:
+            result.append(s)
+            return
+        if left < n:
+            backtrack(s + '(', left + 1, right)
+        if right < left:
+            backtrack(s + ')', left, right + 1)
+    backtrack('', 0, 0)
+    return result
