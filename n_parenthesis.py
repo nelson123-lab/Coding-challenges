2e967eb74c6e9b7fc7763426985f1281c4a8330b
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-19 21:33:27 -0500

    Update n_parenthesis.py

--- n_parenthesis.py
+++ n_parenthesis.py
@@ -15,3 +15,13 @@ class Solution:
                 backtrack(s + ')', left, right + 1)
         backtrack('', 0, 0)
         return result
+"""
+This is a Python code that generates all valid combinations of n pairs of parentheses. 
+The function generateParenthesis takes an integer n as input and returns a list of strings, where each string represents a valid combination of parentheses.
+The function uses a recursive approach to generate all possible combinations of parentheses. 
+It starts with an empty string and keeps adding either an opening or closing parenthesis until it reaches the desired length of 2*n. At each step, 
+it checks if the number of opening parentheses is less than n, and if so, it adds an opening parenthesis. Similarly, 
+it checks if the number of closing parentheses is less than the number of opening parentheses, and if so, it adds a closing parenthesis.
+The function then returns the list of all valid combinations of parentheses. 
+This code is an example of backtracking, which is a common technique used in algorithm design to explore all possible solutions to a problem.
+"""
