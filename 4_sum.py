02938d4e7651d687b401fc43a10c3edc02c4b090
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-26 23:27:05 -0500

    Update 4_sum.py

--- 4_sum.py
+++ 4_sum.py
@@ -8,26 +8,20 @@ def hasQuadruplet(nums, target):
  
     # consider each element except the last element
     for i in range(len(nums) - 1):
- 
-        # start from the i'th element until the last element
+        # start from the i'th element until the last elements
         for j in range(i + 1, len(nums)):
- 
             # calculate the remaining sum
             val = target - (nums[i] + nums[j])
- 
             # if the remaining sum is found on the dictionary,
             # we have found a quadruplet
             if val in d:
- 
                 # check every pair having a sum equal to the remaining sum
                 for pair in d[val]:
                     x, y = pair
- 
                     # if quadruplet doesn't overlap, print it and return true
                     if (x != i and x != j) and (y != i and y != j):
                         print('Quadruplet Found', (nums[i], nums[j], nums[x], nums[y]))
                         return True
- 
             # insert the current pair into the dictionary
             d.setdefault(nums[i] + nums[j], []).append((i, j))
  
