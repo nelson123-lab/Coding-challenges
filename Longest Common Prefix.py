961aa74a91c72657979c8df477c6522d38cf1181
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-27 23:35:01 -0500

    Update Longest Common Prefix.py

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -8,9 +8,12 @@ def longestCommonPrefix(strs):
             if not prefix:
                 return ""
     return prefix
+
 """
+The in function cannot be used here which will result in prefix in some parts of the strings. 
 - The .find() function in Python is a built-in string method that returns the index of the first occurrence of a substring in a string. 
 If the substring is not found, it returns -1.
+
 
 - To find the longest common prefix among an array of strings, we can start by comparing the first character of all the strings. If they are the same, we 
 move on to the next character and continue the comparison. If at any point the characters are not the same, we return the prefix up to that point.
