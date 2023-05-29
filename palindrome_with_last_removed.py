dffbada23199bf85a5552b08e9e25d05ca6a55c3
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-28 23:41:33 -0500

    Update palindrome_with_last_removed.py

--- palindrome_with_last_removed.py
+++ palindrome_with_last_removed.py
@@ -28,3 +28,20 @@ def validPalindrome(s: str) -> bool:
 print(validPalindrome("eddze"))
 
 # This solution exceeds the time limit it can be solved by using two - pointer approach.
+
+def validPalindrome(s: str) -> bool:
+    left, right = 0, len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            # try deleting left character
+            temp1 = s[:left] + s[left+1:right+1]
+            if temp1 == temp1[::-1]:
+                return True
+            # try deleting right character
+            temp2 = s[:right] + s[left:right]
+            if temp2 == temp2[::-1]:
+                return True
+            return False
+        left += 1
+        right -= 1
+    return True
