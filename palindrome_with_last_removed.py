99b3251038c0c79218567f1d73cca9f3ee6f3cd7
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-28 23:26:01 -0500

    m

--- palindrome_with_last_removed.py
+++ palindrome_with_last_removed.py
@@ -1,15 +1,27 @@
-def validPalindrome(self, s: str) -> bool:
+def validPalindrome(s: str) -> bool:
+    a, b = s[:-1], s[1:]
     if s[::-1] == s:
         return True
-    elif s[:-1][::-1] == s[:-1] or s[1:][::-1] == s[1:]:
+    elif a[::-1] == a or b[::-1] == b:
         return True
     else:
-        for i in s[1:-1]:
-            c = s.replace(i,"")
-            if c[::-1] == c:
+        for i in range(1, len(s[1:-1])+1):
+            left, right = i, len(s)-i
+            new_char = ""
+            # convert string to list
+            s_list = list(s)
+
+            # replace character at index
+            s_list[left] = new_char
+
+            # convert list back to string
+            new_s = "".join(s_list)
+            if new_s[::-1] == new_s:
                 return True
-            else:
-                pass
+            s_list[right] = new_char
+            new_s = "".join(s_list)
+            if new_s[::-1] == new_s:
+                return True
         return False
 
-print(validPalindrome("pidbliassaqozokmtgahluruufwbjdtayuhbxwoicviygilgzduudzgligyviciowxbhuyatdjbwfuurulhagtmkozoqassailbdip"))
+print(validPalindrome("eddze"))
