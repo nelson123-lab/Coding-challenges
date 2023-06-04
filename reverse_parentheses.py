d5b6634b6e8b6b39733166388b0f79597149df78
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-04 15:48:40 -0500

    Update reverse_parentheses.py

--- reverse_parentheses.py
+++ reverse_parentheses.py
@@ -8,12 +8,14 @@ def reverseParentheses(s: str) -> str:
         if chars[i] == '(':
             # Appending the index of starting parentheses '(' to the stack
             stack.append(i)
+        # When i becomes ')' each time the elements gets reversed.
         elif chars[i] == ')':
             # Storing the value of the inner most starting parentheses index to j.
             j = stack.pop()
             # Reversing the elements betweent the index from '(' to ')'.
             chars[j:i] = chars[j:i][::-1]
     
+    # Joining the list values by removing the starting and closing parentheses and converting it into a string.
     return ''.join(c for c in chars if c not in '()')
 
 print(reverseParentheses("(u(love)i)"))
