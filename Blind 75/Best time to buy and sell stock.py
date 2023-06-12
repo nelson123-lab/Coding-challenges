63958dd386323ca7aa5c9ae94583f397c4fcd650
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-11 23:59:20 -0500

    Update Best time to buy and sell stock.py

--- Blind 75/Best time to buy and sell stock.py
+++ Blind 75/Best time to buy and sell stock.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def maxProfit(self, prices):
+# Using two pointer approach but in a different way. 
+# Here the order is important it determines the order in which the stocks are priced.
+        l, r, maxP = 0, 1, 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
