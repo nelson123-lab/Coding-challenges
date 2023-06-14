3637d497e57ce98733217fb74ee5f07f143ea43f
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-14 12:50:47 -0500

    m

--- Leetcode 75/Reverse word in a string.py
+++ Leetcode 75/Reverse word in a string.py
@@ -1,12 +1,16 @@
-a = 'hello   world'
-print(a.split(' '))
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s_list = s.split(" ")
+        l,r = 0, len(s_list)-1
+        reversed_list = s_list[::-1]
+        while l<r:
+            if reversed_list[l] == " ":
+                reversed_list.remove(reversed_list[l])
+                l += 1
+            if reversed_list[r] == " ":
+                reversed_list.remove(reversed_list[r])
+                r -= 1
+        return " ".join(reversed_list)
 
-# animals list
-animals = ['cat', 'dog', 'rabbit', 'guinea pig', 'rabbit']
-
-# 'rabbit' is removed
-animals.remove('rabbit')
-
-
-# Updated animals List
-print('Updated animals list: ', animals)
+ans = Solution()
+print(ans.reverseWords("the sky is  blue"))
