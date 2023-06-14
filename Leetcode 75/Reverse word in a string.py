6bb1932180781808f9a4fe6996ba96fe59f919b2
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-14 12:36:37 -0500

    m

---
+++ Leetcode 75/Reverse word in a string.py
@@ -0,0 +1,12 @@
+a = 'hello   world'
+print(a.split(' '))
+
+# animals list
+animals = ['cat', 'dog', 'rabbit', 'guinea pig', 'rabbit']
+
+# 'rabbit' is removed
+animals.remove('rabbit')
+
+
+# Updated animals List
+print('Updated animals list: ', animals)
