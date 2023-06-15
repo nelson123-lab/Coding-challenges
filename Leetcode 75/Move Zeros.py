6b6003b35ecfe780dfeb0e5c9535a151b6b249e5
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-14 23:24:00 -0500

    update

--- Leetcode 75/Move Zeros.py
+++ Leetcode 75/Move Zeros.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        j = 0
+        i, j = 0, 0
         while i < len(nums):
             if nums[i] != 0:
                 nums[i], nums[j] = nums[j], nums[i]
                 j += 1
             i += 1
         return nums
-    
+
+
 ans = Solution()
 print(ans.moveZeroes([0,1,0,3,0,0,12]))
