13306494158ecf7361cf50f3cc224d1d37fe70e9
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-14 23:15:29 -0500

    two pointer

--- Leetcode 75/Move Zeros.py
+++ Leetcode 75/Move Zeros.py
@@ -1,17 +1,16 @@
 class Solution:
-    def moveZeroes(self, nums) -> None:
+    def moveZeroes(self, nums):
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = 0
-        for idx, ele in enumerate(nums):
-            if ele == 0:
-                nums.pop(idx)
-                count += 1
-            else:
-                pass
-        nums = nums + [0]* count
+        i = 0
+        j = 0
+        while i < len(nums):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+            i += 1
         return nums
     
 ans = Solution()
-print(ans.moveZeroes([0,1,0,3,12]))
+print(ans.moveZeroes([0,1,0,3,0,0,12]))
