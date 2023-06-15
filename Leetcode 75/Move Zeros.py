4e9be6e1f1ee1074363f8443c3e5d54358e62c27
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-14 23:26:36 -0500

    move zeros

--- Leetcode 75/Move Zeros.py
+++ Leetcode 75/Move Zeros.py
@@ -11,6 +11,12 @@ class Solution:
             i += 1
         return nums
 
+"""
+The solution uses two pointer approach where j keep track of the zeros and 
+i is used to iterate over the list. 
+Each time a non zero number is swapped places with a zero so that in O(n) time complexity
+we are able to find the solution.
 
+"""
 ans = Solution()
 print(ans.moveZeroes([0,1,0,3,0,0,12]))
