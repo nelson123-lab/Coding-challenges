3fe3550ce3be507576066b0769afceae5a539815
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-15 23:50:01 -0500

    subsequence

---
+++ Leetcode 75/Two pointers/Is subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        prev_index = -1
+        for idx, ele in enumerate(s):
+
+            if ele in t and t.index(ele) > prev_index:
+                prev_index = idx
+            else:
+                return False
+        return True
