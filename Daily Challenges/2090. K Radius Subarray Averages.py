de78230a995fb4494178b5aa0a44bccdee7e3ec8
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-23 09:25:48 -0500

    Update 2090. K Radius Subarray Averages.py

--- Daily Challenges/2090. K Radius Subarray Averages.py
+++ Daily Challenges/2090. K Radius Subarray Averages.py
@@ -1,1 +1,15 @@
+class Solution:
+    def getAverages(self, nums, k):
+        n = len(nums)
+        avgs = [-1] * n
+
+        for i in range(n):
+            start = max(0, i - k)
+            end = min(n - 1, i + k)
+
+            if end - start + 1 >= 2 * k + 1:
+                subarray_sum = sum(nums[start:end+1])
+                avgs[i] = subarray_sum // (2 * k + 1)
+
+        return avgs
 
