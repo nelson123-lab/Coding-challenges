a9969a321f64db667c53223f8153ddc5a4b293d8
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-29 01:38:27 -0500

    solution

--- Leetcode 75/Arrary-strings/443. String Compression.py
+++ Leetcode 75/Arrary-strings/443. String Compression.py
@@ -6,14 +6,18 @@ class Solution:
         while i < length:
             curr_char = chars[i]
             count = 0
+            # Checking for duplicates
             while i< length and chars[i] == curr_char:
                 count += 1
                 i += 1
-
+            # Assigning the duplicates count as strings to the array
             chars[idx] = curr_char
             idx += 1
+            # if ['a', 'b'] we need to return only ['a', 'b'] and not ['a', '1', 'b','2']
             if count > 1:
                 count_str = str(count)
+                # For handling the case when count becomes more than one digit number.
+                # '12' as '1', '2'
                 for ch in count_str:
                     chars[idx] = ch
                     idx += 1
