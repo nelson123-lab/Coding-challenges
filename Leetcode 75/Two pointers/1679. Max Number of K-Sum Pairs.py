ba0f427fd19a758ed4017ca0726cb4420da9ae57
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-01 23:51:59 -0500

    Update 1679. Max Number of K-Sum Pairs.py

--- Leetcode 75/Two pointers/1679. Max Number of K-Sum Pairs.py
+++ Leetcode 75/Two pointers/1679. Max Number of K-Sum Pairs.py
@@ -1,1 +1,17 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        l, r = 0, len(nums)-1
+        nums.sort()
+        count = 0
+        while l<r:
+            if nums[l] + nums[r] == k:
+                count += 1
+                l += 1
+                r -= 1
+            elif nums[l] + nums[r] > k:
+                r -= 1
+            else:
+                l += 1
+            
+        return count
 
