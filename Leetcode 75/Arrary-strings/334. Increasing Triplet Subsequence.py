82030d5fbac3f17902984dbfa1cfbfd74e54311f
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-04 23:28:10 -0500

    Update 334. Increasing Triplet Subsequence.py

--- Leetcode 75/Arrary-strings/334. Increasing Triplet Subsequence.py
+++ Leetcode 75/Arrary-strings/334. Increasing Triplet Subsequence.py
@@ -13,3 +13,6 @@ class Solution:
 nums = [20,100,10,12,5,13]
 a = Solution()
 print(a.increasingTriplet(nums))
+"""
+This soulution fails to meet the i<j<k but returns 3 numbers that meets nums[i]< nums[j] < nums[k].
+"""
