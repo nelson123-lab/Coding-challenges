3eab54ce6377b16794c61ded838f44538a7f3bc0
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-05 19:15:26 -0500

    Update 643. Maximum Average Subarray 1.py

--- Leetcode 75/Sliding window/643. Maximum Average Subarray 1.py
+++ Leetcode 75/Sliding window/643. Maximum Average Subarray 1.py
@@ -1,1 +1,15 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        maxSum = windowSum = sum(nums[:k])
+
+        for i in range(k, len(nums)):
+            windowSum += nums[i] - nums[i-k]
+            maxSum = max(maxSum, windowSum)
+
+        return maxSum/k
 
