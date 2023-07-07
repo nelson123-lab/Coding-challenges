b4df3f21f05a53f555c2d95850b7ba3d36e0d139
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-07 11:35:55 -0500

    Update 1004. Max Consecutive Ones III.py

--- Leetcode 75/Sliding window/1004. Max Consecutive Ones III.py
+++ Leetcode 75/Sliding window/1004. Max Consecutive Ones III.py
@@ -1,1 +1,18 @@
+class Solution:
+    def longestOnes(self, nums, k):
+        left  = right = 0
 
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                k -= 1
+        
+            if k < 0:
+                if nums[left] == 0:
+                    k += 1
+            
+                left += 1
+
+        return right- left + 1
+
+a = Solution()
+print(a.longestOnes([1,1,1,0,0,0,1,1,1,1,0], k = 2))
