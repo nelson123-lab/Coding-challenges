92ade88b31635e73ca695fc3ff899ad4bdd1ded9
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-07 12:01:25 -0500

    Update 1004. Max Consecutive Ones III.py

--- Leetcode 75/Sliding window/1004. Max Consecutive Ones III.py
+++ Leetcode 75/Sliding window/1004. Max Consecutive Ones III.py
@@ -5,7 +5,8 @@ class Solution:
         for right in range(len(nums)):
             if nums[right] == 0:
                 k -= 1
-        
+
+            # The time when 2 zeros are already replaced with ones.
             if k < 0:
                 if nums[left] == 0:
                     k += 1
@@ -16,3 +17,9 @@ class Solution:
 
 a = Solution()
 print(a.longestOnes([1,1,1,0,0,0,1,1,1,1,0], k = 2))
+
+"""
+The logic behind solving this problem is by understanding the technique.
+k value represents the number of 0's that we can replaced with 1. 
+We can keep track of the k value by increasing and decreasing it when there is 1 and 0 respectively.
+"""
