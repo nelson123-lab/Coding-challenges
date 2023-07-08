a20fd41b7d1047b02a7d7250ef8335c6c5c0593f
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-08 11:51:10 -0500

    Update 1732. Find the Highest Altitude.py

--- Leetcode 75/Pre Sum/1732. Find the Highest Altitude.py
+++ Leetcode 75/Pre Sum/1732. Find the Highest Altitude.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        curralt = maxalt = 0
+        for i in range(len(gain)):
+            curralt += gain[i]
+            maxalt = max(curralt, maxalt)
+        return 0 if maxalt < 0 else maxalt
+"""
+Storing the highest altitude each time and finally returing the maximuum altitude point.
+"""
