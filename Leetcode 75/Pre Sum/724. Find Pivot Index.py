812d781bb7689091cecc05f8ee1b98636d586454
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-08 13:11:56 -0500

    Update 724. Find Pivot Index.py

--- Leetcode 75/Pre Sum/724. Find Pivot Index.py
+++ Leetcode 75/Pre Sum/724. Find Pivot Index.py
@@ -7,6 +7,11 @@ class Solution:
         if leftSum == rightSum:
             return 0
         for i in range(1, len(nums)):
+            """
+            [1,7,3,6,5,6]
+               ^
+            When index starts from 1. we add i-1 that is nums[0] to leftSum and substract nums[1] from rightSum.
+            """
             leftSum += nums[i-1]
             rightSum -= nums[i]
             if leftSum == rightSum:
@@ -16,3 +21,8 @@ class Solution:
     
 a = Solution()
 print(a.pivotIndex([1,7,3,6,5,6]))
+
+"""
+Using the principle of sliding window to reduce the complextiy of the algorithm. Instead of checking the leftSum and rightSum for each index we can 
+add and substract the elements from the previous index to the leftSum and the substract the current index value from the rightSum.
+"""
