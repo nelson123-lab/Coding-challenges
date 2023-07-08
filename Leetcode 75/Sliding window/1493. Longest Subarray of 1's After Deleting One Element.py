fd40962bfedc74a6dce97455626596ae58a7ad88
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-07 19:36:25 -0500

    Update 1493. Longest Subarray of 1's After Deleting One Element.py

--- Leetcode 75/Sliding window/1493. Longest Subarray of 1's After Deleting One Element.py
+++ Leetcode 75/Sliding window/1493. Longest Subarray of 1's After Deleting One Element.py
@@ -10,3 +10,9 @@ class Solution:
                     k += 1
                 left += 1
         return right - left
+"""
+Here it is the similar to the problem Max Consecutive one by removing k values. Here the k is 1. 
+The algorithm first checks for 0 and if it is a 0 k value reduces and if it is less than 0. Which means there have been more than one 0 so we need to check
+other subarray. 
+The pointer left checks from the left again whether the elements are 1 or 0 and increases tha value of k when it is 0. 
+"""
