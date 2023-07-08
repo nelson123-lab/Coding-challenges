9d35f5beaf7c7cf5b84dac5e0df4b979c5d5fce0
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-08 13:07:14 -0500

    Update 724. Find Pivot Index.py

--- Leetcode 75/Pre Sum/724. Find Pivot Index.py
+++ Leetcode 75/Pre Sum/724. Find Pivot Index.py
@@ -1,15 +1,18 @@
 class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
+    def pivotIndex(self, nums) -> int:
         leftSum = rightSum = 0
         i = 0
         leftSum = sum(nums[:i])
         rightSum = sum(nums[i+1:])
         if leftSum == rightSum:
             return 0
-        for i in range(len(nums)):
+        for i in range(1, len(nums)):
             leftSum += nums[i-1]
             rightSum -= nums[i]
             if leftSum == rightSum:
                 return i
             else: pass
         return -1
+    
+a = Solution()
+print(a.pivotIndex([1,7,3,6,5,6]))
