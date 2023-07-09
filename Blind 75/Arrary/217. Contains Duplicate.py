1e120a02cd0447be390f0ff9dc5b86549a65269d
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-08 23:58:40 -0500

    Update 217. Contains Duplicate.py

--- Blind 75/Arrary/217. Contains Duplicate.py
+++ Blind 75/Arrary/217. Contains Duplicate.py
@@ -1,1 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return False if len(set(nums)) == len(nums) else True
 
+"""
+- The logic is when the no of occurances of a particular element is more then the length will be greater than a set(array).
+- Set does not contain duplicate elements.
+"""
