c50479f926c04f4d00247907a4f61342b7bee8b9
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-08 19:17:04 -0500

    Update 2215. Find the Difference of Two Arrays.py

--- Leetcode 75/Heap map-set/2215. Find the Difference of Two Arrays.py
+++ Leetcode 75/Heap map-set/2215. Find the Difference of Two Arrays.py
@@ -11,3 +11,9 @@ class Solution:
                 a.remove(i)
             else: pass
         return [a, nums2]
+
+# The same results can be obtained by using set operations
+class Solution:
+    def findDifference(self, nums1, nums2):
+        nums1, nums2 = set(nums1), set(nums2)
+        return [list(nums1-nums2), list(nums2-nums1)]
