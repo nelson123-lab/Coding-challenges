952be9a9223517361e760840f2396b1870b947b7
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-08 23:46:27 -0500

    Update 1207. Unique Number of Occurrences.py

--- Leetcode 75/Heap map-set/1207. Unique Number of Occurrences.py
+++ Leetcode 75/Heap map-set/1207. Unique Number of Occurrences.py
@@ -1,1 +1,15 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        count = []
 
+        for i in set(arr):
+            curr_count = arr.count(i)
+            if curr_count not in count:
+                count.append(curr_count)
+            else:
+                return False
+        return True
+"""
+- Coverting the list to set inorder to reduce the no of elements to iterate.
+- Keeping a count reference and checking whether the no of occurances are already present in the list.
+"""
