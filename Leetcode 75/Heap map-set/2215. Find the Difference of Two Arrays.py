c566413c14a8cb5b4c522280653450b5cfee3a2b
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-08 19:13:49 -0500

    Update 2215. Find the Difference of Two Arrays.py

--- Leetcode 75/Heap map-set/2215. Find the Difference of Two Arrays.py
+++ Leetcode 75/Heap map-set/2215. Find the Difference of Two Arrays.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def findDifference(self, nums1, nums2):
+        nums, nums2 = list(set(nums1)), list(set(nums2))
+      """
+      Making a copy of the list. When elements of a list are removed while iteration it will result in not iterating through all the elements.
+      """
+        a = nums.copy()
+        for i in nums:
+            if i in nums2:
+                nums2.remove(i)
+                a.remove(i)
+            else: pass
+        return [a, nums2]
