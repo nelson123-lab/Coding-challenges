e1de45f2f2daa9cfd58615eb1c8417939726f555
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-09 23:54:44 -0500

    Update 1657. Determine if Two Strings Are Close.py

--- Leetcode 75/Heap map-set/1657. Determine if Two Strings Are Close.py
+++ Leetcode 75/Heap map-set/1657. Determine if Two Strings Are Close.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2) or set(word1) != set(word2): return False
+        else:
+            if sorted([word1.count(i) for i in set(word1)])== sorted([word2.count(i) for i in set(word2)]):
+                return True
+            else: return False
