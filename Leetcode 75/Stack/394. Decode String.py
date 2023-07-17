18a3911deabc05aef090dfa49cb78f5b29b18a26
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-16 23:48:19 -0500

    Update 394. Decode String.py

--- Leetcode 75/Stack/394. Decode String.py
+++ Leetcode 75/Stack/394. Decode String.py
@@ -31,3 +31,16 @@ class Solution(object):
                 stack.append(s[i])
 
         return ''.join(stack)
+
+
+def decode(self, s: str) -> List[str]:
+    decoded_strs = []
+    i = 0
+    while i < len(s):
+        j = i
+        while s[j] != "#":
+            j += 1
+        length = int(s[i:j])
+        decoded_strs.append(s[j + 1:j + length + 1])
+        i = j + length + 1
+    return decoded_strs
