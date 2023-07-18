90dc952ca3ff6ac891bbb7b6a1e99a513914fdb2
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-17 23:57:33 -0500

    Update 27. Remove Element.py

--- Top 150 interview questions/27. Remove Element.py
+++ Top 150 interview questions/27. Remove Element.py
@@ -1,1 +1,15 @@
+class Solution:
+    def removeElement(self, nums, val) -> int:
+        res = []
+        k = 0
+        length = len(nums)
+        for i in range(length):
+            if nums[i] != val:
+                res.append(nums[i])
+            else:
+                k += 1
+        # res[length-k:] = ["_"]*k
+        return res
 
+a = Solution()
+print(a.removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
