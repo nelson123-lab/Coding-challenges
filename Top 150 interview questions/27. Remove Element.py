1c0e5bd65f38ad075f86e0c74653ca1580726dff
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-18 00:29:46 -0500

    Update 27. Remove Element.py

--- Top 150 interview questions/27. Remove Element.py
+++ Top 150 interview questions/27. Remove Element.py
@@ -8,7 +8,23 @@ class Solution:
                 res.append(nums[i])
             else:
                 k += 1
-        return res
+        res.sort()
+        return length-k
 
 a = Solution()
 print(a.removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
+
+"""
+The above solution doesn't work because the val element should be removed inplace here in the above solution a new array is created.
+"""
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        length = len(nums)
+        for i in range(length):
+            if nums[i] == val:
+                nums[i] = float('inf')
+                k += 1
+        #  the sort() method in most programming languages, including Python, cannot handle the special value inf (infinity) directly.
+        nums.sort()
+        return length-k
