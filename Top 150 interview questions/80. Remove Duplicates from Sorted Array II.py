0d0da72bce793dd2c775a2aeb765c058118c0ec6
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-18 03:07:23 -0500

    Update 80. Remove Duplicates from Sorted Array II.py

--- Top 150 interview questions/80. Remove Duplicates from Sorted Array II.py
+++ Top 150 interview questions/80. Remove Duplicates from Sorted Array II.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        ele = nums[0]
+        count = 0
+        k = 0
+        length = len(nums)
+        for i in range(length):
+            if nums[i] == ele:
+                count += 1
+                if count > 2:
+                    nums[i] = float('inf')
+                    k += 1
+            else:
+                count = 1
+                ele = nums[i]
+        nums.sort()
+        return length - k
+"""
+Here a count is initialized to check whether a number is repeated more than twice. If it is repeated it will be replaced by float('inf').
+This makes it easier to be sorted out.
+"""
