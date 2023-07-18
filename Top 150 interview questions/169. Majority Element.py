7309d9eead6be0786ba4a859777f4304d4d1ddab
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-18 04:11:03 -0500

    Update 169. Majority Element.py

--- Top 150 interview questions/169. Majority Element.py
+++ Top 150 interview questions/169. Majority Element.py
@@ -1,1 +1,25 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        curr_ele = 0
 
+        for ele in nums:
+            if count == 0:
+                curr_ele = ele
+            
+            if ele == curr_ele:
+                count += 1
+            else:
+                count -= 1
+        return curr_ele
+"""
+The above code uses Moore voting algorithm to find the majority element in the array.
+"""
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+      return sorted(nums)[len(nums)//2)
+
+"""
+The same results can be obtained by sorting.
+"""
