26ed22d30af0e27e84c173ec077d8ef4b26731ec
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-18 17:24:03 -0500

    Update 122. Best Time to Buy and Sell Stock II.py

--- Top 150 interview questions/122. Best Time to Buy and Sell Stock II.py
+++ Top 150 interview questions/122. Best Time to Buy and Sell Stock II.py
@@ -1,1 +1,18 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        left, right, total_profit = 0, 1, 0
+        while right< len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                total_profit += profit
+                left = right
+            else:
+                left = right
+            right += 1
+        return total_profit
 
+"""
+This is a similar to finding the max profit of stock. Here maximum profit should be obtained by continuesly buying and selling stock.
+The logic is that whenever there is a profit we sell the stock and buy the stock on the same day.
+We add all the profit to the total profit each time when there is a profit.
+"""
