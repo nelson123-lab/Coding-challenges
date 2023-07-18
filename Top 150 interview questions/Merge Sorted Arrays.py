e2b9c9600ccee05a5d63fb9275a11d1b74ed5f82
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-17 23:30:14 -0500

    Update Merge Sorted Arrays.py

--- Top 150 interview questions/Merge Sorted Arrays.py
+++ Top 150 interview questions/Merge Sorted Arrays.py
@@ -8,4 +8,9 @@ class Solution:
 
 """
 Assigning the values of nums2 into nums1 after the elements in the nums1.
+
+Similar results can be obtained by iterating through nums1 and updating the values nums1.
+
+for i in range(m,m+n):
+    nums1[i]=nums2[i-m]
 """
