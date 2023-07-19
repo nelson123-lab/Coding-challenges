ec6d0752ffd5a58b5331a70b1dcc3bbc4a1472cb
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-19 14:50:49 -0500

    Update 55. Jump Game.py

--- Top 150 interview questions/55. Jump Game.py
+++ Top 150 interview questions/55. Jump Game.py
@@ -12,3 +12,21 @@ class Solution(object):
 The overall idea here is that if the first element of the array is 0 we can't jump to any other array and it will directly return False.
 It keeps track of the maximum number of steps that can be taken from the current position using the curr variable.
 """
+
+# A DP Approach
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if(i+nums[i]>=goal):
+                #it is possible to reach to the goal...
+                goal=i
+        return goal==0
+
+"""
+The time complexity of this program is O(n), where n is the length of the input list `nums`. This is because the program iterates through the list once in the `for` loop, checking each element to determine if it can reach the goal. The loop runs in reverse order, starting from the second-to-last element and ending at the first element, so it visits each element once.
+
+The space complexity of this program is O(1), which means it uses constant space. This is because the program only uses a fixed amount of additional space to store the `goal` variable and the loop index `i`. The space used does not depend on the size of the input list `nums`.
+"""
