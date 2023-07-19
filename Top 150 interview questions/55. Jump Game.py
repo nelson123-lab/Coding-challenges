1bd79bd6ca0b40124dc662f5985387b1775725c8
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-18 23:14:15 -0500

    Update 55. Jump Game.py

--- Top 150 interview questions/55. Jump Game.py
+++ Top 150 interview questions/55. Jump Game.py
@@ -1,1 +1,10 @@
+class Solution(object):
+    def canJump(self, nums):
+        curr = nums[0]
+        for i in range(1,len(nums)):
+            if curr == 0:
+                return False
+            curr -= 1
+            curr = max(curr, nums[i])     
+        return True
 
