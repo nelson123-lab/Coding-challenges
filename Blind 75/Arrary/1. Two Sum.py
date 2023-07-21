08cc692b87765741523e7d39b4080c762ff92ed6
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-21 00:14:30 -0500

    Update 1. Two Sum.py

--- Blind 75/Arrary/1. Two Sum.py
+++ Blind 75/Arrary/1. Two Sum.py
@@ -1,5 +1,5 @@
 # To find the indexes of two elements that add up to the target.
-
+# Brutforce Approach
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Iterating through the number list.
@@ -13,3 +13,23 @@ class Solution:
                 return [i, nums.index(a)]
             else:
                 pass
+"""
+This solution passes all the test case but the time complexity is O(n^2). Here nested for loop is present. The in and .index operation works in 
+O(n) time complexity and it is within the for loop. Therefore, O(n^2).
+"""
+# Optimal Approach
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ref = {}
+        for idx, ele in enumerate(nums):
+            diff = target - ele
+            if diff in ref:
+                return [ref[diff], idx]
+            ref[ele] = idx
+        return
+"""
+Time Complexity O(n)
+Here a dictionary is used within the forloop. The insertion into the dictionary and search happens in constant time.
+Space Complextiy O(n)
+Here the space used depend on the input. The worst case when no two elements add up to the target all the elements will stored in the dictionary.
+"""
