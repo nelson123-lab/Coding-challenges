69933857469ce48b2298bfb7337ade0d6b56398f
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-22 21:41:24 -0500

    Update 274. H-Index.py

--- Top 150 interview questions/274. H-Index.py
+++ Top 150 interview questions/274. H-Index.py
@@ -1,1 +1,19 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        low, high, mid = 0, n-1, 0
+        citations.sort()
+        while low <= high:
+            mid = low + (high-low)//2
+            if citations[mid] == (n-mid):
+                return citations[mid]
+            elif citations[mid] > (n-mid):
+                high = mid-1
+            else:
+                low = mid+1
+        return n-low
 
+"""
+This problem is solved using binary search. The array is first sorted in ascending order.
+"""
+            
