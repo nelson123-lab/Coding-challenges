60269128e2ea364c28818aae9749c52076bc63a3
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-22 22:43:32 -0500

    Update 274. H-Index.py

--- Top 150 interview questions/274. H-Index.py
+++ Top 150 interview questions/274. H-Index.py
@@ -14,6 +14,12 @@ class Solution:
         return n-low
 
 """
-This problem is solved using binary search. The array is first sorted in ascending order.
+- This problem is solved using binary search. The array is first sorted in ascending order.
+- For H index to be valid there should be same no of papers published by the author with the same or more number of citations. Here the first 
+condition checks that. This only works after sorting the algorithm.
+- The final part returns the maximum value of h-index among the array the value.
+eg: [1,2,4,6,1,5]
+In this example after the iterations the high will become 2 and low will become 3 that's when the while loop stops. The algorithm will 
+return n-low => 6-3 = 3. Here it is returning the highest value that h index can obtain.
 """
             
