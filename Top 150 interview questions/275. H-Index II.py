fc0f46cc187786fa05b01d4943cb0c918ca1388d
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-22 23:42:42 -0500

    Update 275. H-Index II.py

--- Top 150 interview questions/275. H-Index II.py
+++ Top 150 interview questions/275. H-Index II.py
@@ -1,1 +1,23 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        low, high, mid = 0, n-1, 0
+        while low <= high:
+            mid = low + (high-low)//2
+            if citations[mid] == (n-mid):
+                return citations[mid]
+            elif citations[mid] > (n-mid):
+                high = mid-1
+            else:
+                low = mid+1
+        return n-low
 
+"""
+The only difference between the H-index and H-index II problem is that the array is sorted in ascending order by default. The algorithm is
+exactly similar.
+
+Time Complexity O(logn)
+- This is due to the use of binary search through the citations array. This uses a logarithmic time complexity.
+Space Complexity O(1)
+- This algorithm works in constant time. There O(1).
+"""
