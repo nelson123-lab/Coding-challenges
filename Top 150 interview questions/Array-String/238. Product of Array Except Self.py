713d4735f5e84165660d7730e5d613677f5db8c0
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-25 23:59:22 -0500

    Update 238. Product of Array Except Self.py

--- Top 150 interview questions/Array-String/238. Product of Array Except Self.py
+++ Top 150 interview questions/Array-String/238. Product of Array Except Self.py
@@ -12,3 +12,10 @@ class Solution:
             postfix *= nums[i]
         return result
 
+"""
+Since we are not able to use the "/" operator. It becomes harder to do this problem in O(n) time complexity. 
+This solution works in O(n) since there are no nested for loops.
+There is trick you need to know inorder to solve this problem.
+Reference:-
+
+"""
