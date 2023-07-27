88f48e42d79ccc3a8f3a6e5272c21f705eb2f3e8
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-27 09:20:07 -0500

    Update 134. Gas Station.py

--- Top 150 interview questions/Array-String/134. Gas Station.py
+++ Top 150 interview questions/Array-String/134. Gas Station.py
@@ -11,3 +11,13 @@ class Solution:
                 total = 0
                 start = i+1
         return start 
+
+"""
+- This is a greedy approach. Here we check whether the total cost of gas for travel is greater than the gas available at the station in total. If this is 
+not available, then there is no starting position.
+- The next step we are doing is adding up the remaining gas after each station. If the gas becomes negative we start from the next index. 
+- We don't need to worry about getting index out of range. There will be a solution before that or it will be the last index. 
+
+Time Complexity O(1)
+Space Complexity O(1)
+"""
