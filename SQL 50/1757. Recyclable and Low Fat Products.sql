c11a570122d70f6e4890d63e90bca690e4a9691b
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-26 22:30:12 -0500

    Update 1757. Recyclable and Low Fat Products.sql

--- SQL 50/1757. Recyclable and Low Fat Products.sql
+++ SQL 50/1757. Recyclable and Low Fat Products.sql
@@ -1,4 +1,5 @@
 SELECT product_id FROM Products
 WHERE low_fats = 'Y' AND recyclable = 'Y'
 
-/* Here we need the product_id with a requirement that */
+/* Here we need the product_id with a requirement that low_fats is True and recyclable is True.
+We use AND Function obtaining both.*/
