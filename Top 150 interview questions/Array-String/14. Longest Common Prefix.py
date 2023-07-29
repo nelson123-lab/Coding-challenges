2f60db2c0ff4d91c1aab74cd8d4aabbbc964df2c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-29 02:52:37 -0500

    Update 14. Longest Common Prefix.py

--- Top 150 interview questions/Array-String/14. Longest Common Prefix.py
+++ Top 150 interview questions/Array-String/14. Longest Common Prefix.py
@@ -1,1 +1,20 @@
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            print(strs[i].find(prefix))
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
 
+"""
+The time complexity of the given program is O(n * m), where n is the length of the input list strs and m is the average length 
+of the strings in strs. This is because the program iterates through each character of the strings in strs to find the 
+longest common prefix.
+
+Space Complexity O(1)
+"""
