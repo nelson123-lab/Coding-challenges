07c1ed75b5977ed390134f76645f5b770cbaa8fe
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-28 23:37:59 -0500

    Update 58. Length of Last Word.py

--- Top 150 interview questions/Array-String/58. Length of Last Word.py
+++ Top 150 interview questions/Array-String/58. Length of Last Word.py
@@ -1,1 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
 
+"""
+This is a simple problem. It returns the last words length. In python, if you index lst[-1] it will return the last element.
+Time Complexity O(n)
+- The split method uses O(n) time complexity to make the split.
+Space COmplexity O(1)
+- No space that is depended on the input is used.
+"""
