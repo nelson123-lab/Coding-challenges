fb5cbd716c9acf0f30d16e47d6be77c15cd1e299
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-29 03:36:14 -0500

    Update 151. Reverse Words in a String.py

--- Top 150 interview questions/Array-String/151. Reverse Words in a String.py
+++ Top 150 interview questions/Array-String/151. Reverse Words in a String.py
@@ -1,1 +1,11 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        string = s.split()
+        return " ".join(string[::-1])
 
+"""
+Time Complexity O(n)
+- The split() method takes O(n) time complexity, the reversing also takes O(n) time Complexity. When combined O(n)
+Space Complexity O(n)
+- Here when split happens the list formed uses space according to the input size.
+"""
