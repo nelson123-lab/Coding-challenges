fd3c32374dc59730c42b49ac9bde05c57e73db14
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-07-31 23:30:43 -0500

    Update 1148. Article Views I.sql

--- SQL 50/SELECT/1148. Article Views I.sql
+++ SQL 50/SELECT/1148. Article Views I.sql
@@ -1,1 +1,4 @@
+# Write your MySQL query statement below
+select distinct author_id as id from views where author_id = viewer_id order by id
 
+/* Here distinct authors are taken as duplicates were present. The authors with authors id and viewers id are selected id are ordered by authors_id */
