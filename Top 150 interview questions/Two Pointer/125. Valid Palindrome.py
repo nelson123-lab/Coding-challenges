8ed8a2d5f577dca3bfb8add8e4c33ba0f880582d
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-03 12:36:18 -0500

    Update 125. Valid Palindrome.py

--- Top 150 interview questions/Two Pointer/125. Valid Palindrome.py
+++ Top 150 interview questions/Two Pointer/125. Valid Palindrome.py
@@ -5,3 +5,10 @@ class Solution:
             if i.isalnum():
                 new += i.lower()
         return new == new[::-1]
+
+"""
+Time Complexity is O(n)
+- It's because the .isalnum() works in O(1) time complexity. It uses bitmap character classification table to check whether the string contains all characters or not.
+Space Complexity is O(n)
+- Space is used according to the input size in the variable new.
+"""
