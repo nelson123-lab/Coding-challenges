eab047eb68cb2809de7aea2872e9bd49ec80e7db
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-04 04:38:12 -0500

    Update 167. Two Sum II - Input Array Is Sorted.py

--- Top 150 interview questions/Two Pointer/167. Two Sum II - Input Array Is Sorted.py
+++ Top 150 interview questions/Two Pointer/167. Two Sum II - Input Array Is Sorted.py
@@ -1,1 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i, j = 0, len(nums)-1
+        while i < j:
+            s = nums[i] + nums[j]
+            if s == target:
+                return [i+1, j+1]
+            if s > target:
+                j-= 1
+            else:
+                i += 1    
+        return []
 
+"""
+Here two pointers are added and checks whether the sums equals to target.
+Time Complexity O(n)
+Space Complexity O(1)
+
+"""
