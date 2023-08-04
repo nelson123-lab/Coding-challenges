8b22040ac7ece937949d3a4fb4ae65cab5e44c49
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-03 23:26:22 -0500

    Update 392. Is Subsequence.py

--- Top 150 interview questions/Two Pointer/392. Is Subsequence.py
+++ Top 150 interview questions/Two Pointer/392. Is Subsequence.py
@@ -1,1 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        p, q = 0, 0 # represents the pointers for s and t respectively.
+        if len(s) > len(t):
+            return False
+
+        while p < len(s) and q < len(t):
+            if s[p] == t[q]: # Checking for same characters existence in both the strings.
+                p += 1
+            q += 1
+        return p == len(s) # Checking if all the characters of s has been iterated.
 
