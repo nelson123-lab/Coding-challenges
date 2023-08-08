d3db36d26d326f9c39195d5a81c19c915195472e
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-08 13:52:30 -0500

    Update 15. 3Sum.py

--- Top 150 interview questions/Two Pointer/15. 3Sum.py
+++ Top 150 interview questions/Two Pointer/15. 3Sum.py
@@ -1,25 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
+        res = set()
 
-        ans = set()
-        for i in range(n):
+        for i in range(len(nums)):
             j = i + 1
-            k = n - 1
-
-            while j<k:
+            k = len(nums)-1
+            while j < k:
                 if nums[i] + nums[j] == -nums[k]:
-                    ans.add((nums[i], nums[j], nums[k]))
+                    res.add((nums[i], nums[j], nums[k]))
                     j += 1
                     k -= 1
-                elif nums[i] + nums[j] + nums[k] < 0:
-                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
                 else:
-                    k -= 1
-        return ans
+                    j += 1
+        return res
 
 """
 Time Complexity O(n^2)
-Space Complexity O(1)
+- Here the while loop is iterating inside the for loop which results in this time complexity.
+Space Complexity O(n)
+- Here a set is used to store the information which can be upto the input size.
 """
