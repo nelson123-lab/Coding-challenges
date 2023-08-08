5141d354a279f4e30595d66cef0847da94a3a04d
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-08 13:51:57 -0500

    Update 15. 3Sum.py

--- Blind 75/Arrary/15. 3Sum.py
+++ Blind 75/Arrary/15. 3Sum.py
@@ -1,1 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = set()
 
+        for i in range(len(nums)):
+            j = i + 1
+            k = len(nums)-1
+            while j < k:
+                if nums[i] + nums[j] == -nums[k]:
+                    res.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return res
+
+"""
+Time Complexity O(n^2)
+- Here the while loop is iterating inside the for loop which results in this time complexity.
+Space Complexity O(n)
+- Here a set is used to store the information which can be upto the input size.
+"""
