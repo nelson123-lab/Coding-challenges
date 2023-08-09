a3e4795cb7fcbd3df42df7247ca24a59f8f4122c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-09 17:05:35 -0500

    Update 1378. Replace Employee ID With The Unique Identifier.sql

--- SQL 50/BASIC JOINS/1378. Replace Employee ID With The Unique Identifier.sql
+++ SQL 50/BASIC JOINS/1378. Replace Employee ID With The Unique Identifier.sql
@@ -1,2 +1,7 @@
-# Write your MySQL query statement below
+/* Write your MySQL query statement below */
 select EU.unique_id, E.name from Employees as E left join EmployeeUNI as EU ON EU.id = E.id
+
+/* Here the EmployeeUNI table is represented as EU and Employees table as E.
+We are using left join because we need all the user information irrespective of whether of them present in the EU table. The left join works in the same way. 
+It will return all the employees of left table irrespective of whether they have a matching id in EU table or not. */
+
