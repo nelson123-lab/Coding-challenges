4b7ed6aec705a601e7d1ec9828eb31ce0c683591
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-09 23:58:20 -0500

    Update 76. Minimum Window Substring.py

--- Top 150 interview questions/Sliding Window/76. Minimum Window Substring.py
+++ Top 150 interview questions/Sliding Window/76. Minimum Window Substring.py
@@ -1,1 +1,11 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        window_start = 0
+        min_len = 0
+        curr_sum = 0
+        s_i, s_e = 0, 0
 
+        for window_end in range(len(nums)):
+            curr_sum += nums[window_end]
+
+            while curr_sum > target
