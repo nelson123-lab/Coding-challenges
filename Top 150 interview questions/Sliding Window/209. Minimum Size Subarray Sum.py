a317ed7e9124313345a9bb9692db4679c9e1b394
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-10 23:53:54 -0500

    Update 209. Minimum Size Subarray Sum.py

--- Top 150 interview questions/Sliding Window/209. Minimum Size Subarray Sum.py
+++ Top 150 interview questions/Sliding Window/209. Minimum Size Subarray Sum.py
@@ -12,3 +12,9 @@ class Solution:
                 left += 1
         return 0 if res == float('inf') else res
                 
+"""
+- Here 2 pointers act as the start and end of the sliding window. The values are added to the total sum each time and compares with the target. 
+- When the total sum is less than target we add the next element else we substracts the starting element of the sliding window and checks again.
+Time Complexity O(n)
+Space Complexity O(1)
+"""
