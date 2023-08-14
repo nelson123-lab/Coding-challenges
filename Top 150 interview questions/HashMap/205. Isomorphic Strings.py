ae38863e8da3f58cc54a8e9027daddb847e8d5a5
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-13 23:59:09 -0500

    Update 205. Isomorphic Strings.py

--- Top 150 interview questions/HashMap/205. Isomorphic Strings.py
+++ Top 150 interview questions/HashMap/205. Isomorphic Strings.py
@@ -1,1 +1,22 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        d = {}
+        p = set()
+        for i in range(len(s)):
+            x, y = s[i], t[i]
+            if x in d:
+                if d[x] != y:
+                    return False
+            else:
+                if y in p:
+                    return False
+                d[x] = y
+                p.add(y)
+        return True
 
+"""
+Time Complexity O(n)
+Space Complexity O(n)
+"""
