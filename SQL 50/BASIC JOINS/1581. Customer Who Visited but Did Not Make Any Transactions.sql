e85300f0e3ac536d95af51776ebf4932acd64b29
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-14 23:52:49 -0500

    Update 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- SQL 50/BASIC JOINS/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ SQL 50/BASIC JOINS/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -5,3 +5,7 @@ LEFT JOIN Transactions T
 ON V.visit_id = T.visit_id
 WHERE T.Transaction_id IS NULL
 GROUP BY V.customer_id;
+
+
+/* Here LEFT JOIN is used to get all the visitors. The Visitors with Transactions = NuLL are the visitors that haven't made any transaction after visting the mall.
+We need to find the count of these specific people. */
