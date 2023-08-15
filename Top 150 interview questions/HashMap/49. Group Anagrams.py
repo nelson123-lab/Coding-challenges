1008b6915f7ef0997cab4094984c2cb49ee0c752
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-15 12:23:34 -0500

    Update 49. Group Anagrams.py

--- Top 150 interview questions/HashMap/49. Group Anagrams.py
+++ Top 150 interview questions/HashMap/49. Group Anagrams.py
@@ -1,1 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
 
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            hashmap[sorted_word].append(word)
+
+        return list(hashmap.values())
