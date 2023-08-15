d6ba51d202bdc9cdfe1b3342895cea4b0860eb4d
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-15 16:21:09 -0500

    Update 1. Two Sum.py

--- Top 150 interview questions/HashMap/1. Two Sum.py
+++ Top 150 interview questions/HashMap/1. Two Sum.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = {}
+        for  idx in range(len(nums)):
+            diff = target - nums[idx]
+            if diff in res and res[diff] != idx:
+                return res[diff], idx
+            else:
+                res[nums[idx]] = idx
+        return []
+"""
+Time Complexity O(n)
+- We are iterating through the loop one time.
+Space Complexity O(n)
+- We use n space according to the input in the worst case for the hashmap.
+"""
