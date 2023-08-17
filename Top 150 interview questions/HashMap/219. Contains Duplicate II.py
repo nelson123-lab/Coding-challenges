5842adf36ea3982e4b17d2988fee2400bef3309c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-16 22:39:24 -0500

    Update 219. Contains Duplicate II.py

--- Top 150 interview questions/HashMap/219. Contains Duplicate II.py
+++ Top 150 interview questions/HashMap/219. Contains Duplicate II.py
@@ -1,1 +1,15 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+        for idx in range(len(nums)):
+            if nums[idx] in hashmap and abs(idx-hashmap[nums[idx]]) <= k:
+                return True
+            hashmap[nums[idx]] = idx
+        return False
 
+"""
+Time Complexity O(n)
+- Here only 1 iteration is happening, so the time complexity is O(n)
+Space Complexity O(n)
+- We are using a hashmap to keep track of the elements within the list. It is O(min(n, k) but the worst case space complexity is O(n).
+"""
