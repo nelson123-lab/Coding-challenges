69c8fbd5591beb19d68aa365861372ff84b83355
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-19 16:27:49 -0500

    Update 12. Integer to Roman.py

--- Top 150 interview questions/Array-String/12. Integer to Roman.py
+++ Top 150 interview questions/Array-String/12. Integer to Roman.py
@@ -1,1 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        int_roman = {1000: "M", 900: "CM", 500: "D", 400: 'CD', 100: "C", 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: "IV", 1: "I"}
+        res = ""
+        for i in int_roman:
+            while num>=i:
+                num-= i
+                res += int_roman[i]
+        return res
 
+"""
+Time Complexity O(n)
+- Here n is the number of intergers inside the num.
+Space Complexity O(1)
+- Uses constant amount of additional space.
+"""
