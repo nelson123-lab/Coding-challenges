891ba7f05c5b2f21dcab97a5c8640ea7996fe673
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-20 18:28:39 -0500

    Update Subarray Sum.py

--- Practice Problems/Subarray Sum.py
+++ Practice Problems/Subarray Sum.py
@@ -2,6 +2,13 @@
 There are 3 methods of finding the subarray over here. 
 """
 class Solution:
+    # This is the brute force approach we are finding all the subarrays and finding the sum of each subarray.
+    """
+    Time Complexity O(n^3)
+    - This is because the sum() operation takes O(n) time complexity to find the sum of an array. There are 2 nested for loops as well.
+    Space Complexity O(1)
+    - Only one variable is used to storing the data.
+    """
     def SubArraySum1(self, arr):
         # subarrays = []
         totalSum = 0
@@ -11,7 +18,12 @@ class Solution:
                 totalSum += sum(arr[no:j +1])
         return totalSum
     
-    
+    """
+    Time Complexity O(n^2)
+    - Here we eliminated the use of sum() operation thus reducing the time complexity to O(n^2)
+    Space Complexity O(1)
+    - No additional space is used here.
+    """
     def SubArraySum2(self, arr):
         n = len(arr)
         temp, result = 0, 0
@@ -23,7 +35,13 @@ class Solution:
                 temp += arr[j]
                 result += temp
         return result
-    
+
+    """
+    Time Complexity O(n)
+    - This is the optimal solution which works using mathematical formula of finding the sum of all subarray starting with start, end index each time.
+    Space Complexity O(1)
+    - No additional space is used by the algorithm.
+    """
     def SubArraySum3(self, arr):
         totalSum = 0
         n = len(arr)
