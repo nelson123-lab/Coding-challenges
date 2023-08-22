aabd2c494b7d7691c4940ac44178d17d72346401
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-22 15:04:10 -0500

    Update 20. Valid Parentheses.py

--- Top 150 interview questions/Stack/20. Valid Parentheses.py
+++ Top 150 interview questions/Stack/20. Valid Parentheses.py
@@ -1,21 +1,54 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        dictionary = {"{": "}", "[": "]", "(": ")"}
+        dict = {'(': ')', '{': '}', '[' :']'}
 
-        brackets_array = [bracket for bracket in s]
-
-        for b in brackets_array:
-
-            if b in '([{':
+        brackets = list(s)
+        for b in brackets:
+            if b in '({[':
                 stack.append(b)
-            elif b in '}])':
+            elif b in ']})':
                 if not stack:
                     return False
-                last_bracket = stack[-1]
-                if dictionary[last_bracket] == b:
+                lastBracket = stack[-1]
+                if dict[lastBracket] == b:
                     stack.pop()
                 else:
                     return False
-        
         return len(stack) == 0
+"""
+Time Complexity O(n^2)
+- Here the O(n) time complexity is due to the opeartion of " if b in '({['. It works within a for loop. Which results in overall time compelxity of O(n^2)
+Space Complexity O(n)
+- The space compelxity is due to the bracket list that is created according to the input. The stack list can also contribute to the space complexity but it's mainly due to the bracket list.
+"""
+
+"""
+The above solution can be optimized to O(n) time compelxity by making some modifications.
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dict = {'(': ')', '{': '}', '[' :']'}
+        brackets = list(s)
+        setA = set('({[')
+        setB = set(']})')
+        for b in brackets:
+            if b in setA:
+                stack.append(b)
+            elif b in setB:
+                if not stack:
+                    return False
+                lastBracket = stack[-1]
+                if dict[lastBracket] == b:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
+"""
+Time Complexity O(n)
+- Here the time complexity reduced to O(n) since checking whether an element exists in set is an O(1) operation.
+Space Complexity O(n)
+- The space complexity is same as above.
+"""
