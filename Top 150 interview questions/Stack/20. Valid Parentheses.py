daf077ef5e4e9f18c50b582f80f25c873dd9a899
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-21 23:51:50 -0500

    Update 20. Valid Parentheses.py

--- Top 150 interview questions/Stack/20. Valid Parentheses.py
+++ Top 150 interview questions/Stack/20. Valid Parentheses.py
@@ -1,1 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dictionary = {"{": "}", "[": "]", "(": ")"}
 
+        brackets_array = [bracket for bracket in s]
+
+        for b in brackets_array:
+
+            if b in '([{':
+                stack.append(b)
+            elif b in '}])':
+                if not stack:
+                    return False
+                last_bracket = stack[-1]
+                if dictionary[last_bracket] == b:
+                    stack.pop()
+                else:
+                    return False
+        
+        return len(stack) == 0
