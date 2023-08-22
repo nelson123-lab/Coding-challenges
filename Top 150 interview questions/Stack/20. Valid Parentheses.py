dd8e22d7e892ef3ca8e06ed88aaa0ff323b093ea
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-22 16:18:00 -0500

    Update 20. Valid Parentheses.py

--- Top 150 interview questions/Stack/20. Valid Parentheses.py
+++ Top 150 interview questions/Stack/20. Valid Parentheses.py
@@ -19,6 +19,7 @@ class Solution:
 """
 Time Complexity O(n^2)
 - Here the O(n) time complexity is due to the opeartion of " if b in '({['. It works within a for loop. Which results in overall time compelxity of O(n^2)
+- A list is used to iterate instead of string as it iterates faster.
 Space Complexity O(n)
 - The space compelxity is due to the bracket list that is created according to the input. The stack list can also contribute to the space complexity but it's mainly due to the bracket list.
 """
@@ -30,10 +31,9 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         dict = {'(': ')', '{': '}', '[' :']'}
-        brackets = list(s)
         setA = set('({[')
         setB = set(']})')
-        for b in brackets:
+        for b in s:
             if b in setA:
                 stack.append(b)
             elif b in setB:
@@ -50,5 +50,32 @@ class Solution:
 Time Complexity O(n)
 - Here the time complexity reduced to O(n) since checking whether an element exists in set is an O(1) operation.
 Space Complexity O(n)
-- The space complexity is same as above.
+- The space complexity is same as above even though there is no bracket list used. Here the stack can be n length if there are only starting brackets within the
+string s. This space complexity can be improved further by adding a condition that if there is one more character after half of the characters are opening bracket in stack then return False.
+Since we half of the string are opening bracket then an equal amount of closing brackets are required for this string be a valid parenthesis. 
 """
+
+"""
+Here we can even use deque instead of list to acheive the same objective.
+"""
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        my_deque = deque()
+
+        dict = {'(': ')', '{': '}', '[' :']'}
+        setA = set('({[')
+        setB = set(']})')
+        for b in s:
+            if b in setA:
+                my_deque.append(b)
+            elif b in setB:
+                if not my_deque:
+                    return False
+                lastBracket = my_deque[-1]
+                if dict[lastBracket] == b:
+                    my_deque.pop()
+                else:
+                    return False
+        return len(my_deque) == 0
