9825bb50b399cb5a580239b8a7eea77ba4391295
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-23 23:47:42 -0500

    Update 300. Longest Increasing Subsequence.py

--- Blind 75/Dynamic Programming/300. Longest Increasing Subsequence.py
+++ Blind 75/Dynamic Programming/300. Longest Increasing Subsequence.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        LIS=[1]*n
+        
+        for i in range(n,-1,-1):
+            for j in range(i+1,n):
+                if nums[j] > nums[i]:
+                    LIS[i]=max((LIS[i]), (LIS[j]+1))
+        
+        return max(LIS)
