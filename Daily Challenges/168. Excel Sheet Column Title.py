c7796e8cf1470ed00706f3f31d3048b88a77083c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-23 23:48:38 -0500

    Update 168. Excel Sheet Column Title.py

--- Daily Challenges/168. Excel Sheet Column Title.py
+++ Daily Challenges/168. Excel Sheet Column Title.py
@@ -1,26 +1,14 @@
-# class Solution:
-#     def convertToTitle(self, columnNumber: int) -> str:
-#         alphabet_dict = {}
-#         for i in range(ord('A'), ord('Z')+1):
-#             alphabet_dict[i - ord('A') + 1] = chr(i)
-#         if columnNumber == 1:
-#             return alphabet_dict[columnNumber]
-#         else:
-#             total = 0
-#             for i in columnNumber:
-#                 total += alphabet_dict[i]
-#             return total
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         ans = ""
 
         while columnNumber:
-            tmp = columnNumber % 26
-            if tmp == 0:
-                tmp = 26  # chr(26) is Z
-                columnNumber -= 26
+            temp = columnNumber % 26
 
-            c = chr(ord('A') + tmp - 1)            
+            if temp == 0:
+                temp = 26
+                columnNumber -= 26
+            c = chr(ord('A') + temp - 1)
             ans = c + ans
 
             columnNumber //= 26
