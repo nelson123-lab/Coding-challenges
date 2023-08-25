8da06b5586bc49067d215074d060d3dbc539d4a6
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-24 23:54:40 -0500

    Update 168. Excel Sheet Column Title.py

--- Daily Challenges/168. Excel Sheet Column Title.py
+++ Daily Challenges/168. Excel Sheet Column Title.py
@@ -19,4 +19,25 @@ Space complexity O(log(columnNumber)
 - The space is also logarithmic since the ans variable is dependent on the input.
 """
 
+"""
+The above solution can be optimized for faster operation.
+"""
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        ans = ""
+        
+        while columnNumber > 0:
+            columnNumber -= 1
+            ans = chr(columnNumber % 26 + ord('A')) + ans
+            columnNumber //= 26
+        
+        return ans
+"""
+Time Complexity O(1)
+- Here the time complexity is reduced to O(1)
+Space Complexity O(log(columnNumber))
+- The space complexity remains the same.
+"""
+
 
