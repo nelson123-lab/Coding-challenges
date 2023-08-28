7692954156787eb925245e2c07111334534c80a9
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-27 21:46:10 -0500

    Update 595. Big Countries.py

--- 30 days of Pandas/Data Filtering/595. Big Countries.py
+++ 30 days of Pandas/Data Filtering/595. Big Countries.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    # Here a new dataframe is created with the name bigCountries based on the condition 1 or Condition 2.
+    bigCountries = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+    # Returing the result with only showing the required columns of the dataframe.
+    return bigCountries[['name', 'population', 'area']]
+
+"""
+Here the syntax when you have 2 condition for two columns
+bigCountries = df[(df['column1'] >= 3000000) | (df['column2'] >= 25000000)]
+bigCountries = df[(condition A) | (condition B)]  - This will create a new dataframe that contains all the columns values that satisfies the conditions.
+"""
