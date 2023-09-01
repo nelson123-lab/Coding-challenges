ecf05d1e215c027037895a21921a0e189a239501
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-31 19:51:02 -0500

    Update 1757. Recyclable and Low Fat Products.py

--- 30 days of Pandas/Data Filtering/1757. Recyclable and Low Fat Products.py
+++ 30 days of Pandas/Data Filtering/1757. Recyclable and Low Fat Products.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    products1 = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return products1[['product_id']]
+
+"""
+Here we extract the data that satisfies both the condition to the products1 and return the product_id column
+"""
