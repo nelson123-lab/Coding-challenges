e77f386c75a6535673b6409f152413a096fcebe8
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-31 23:52:38 -0500

    Update 649. Dota2 Senate.py

--- Leetcode 75/Queue/649. Dota2 Senate.py
+++ Leetcode 75/Queue/649. Dota2 Senate.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        q = deque(senate)
+        votes = {"D":1, "R":-1}
+        count = 0
+        while abs(count) < len(q):
+            v = q.popleft()
+            count += votes[v]            
+            if count > 0 and v == "D" or count<0 and v == "R":
+                q.append(v)     
+        return 'Dire' if count>0 else  'Radiant'
