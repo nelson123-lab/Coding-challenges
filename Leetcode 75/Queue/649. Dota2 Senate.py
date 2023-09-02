503a5ed7627d2360f9df935da0311ba8920b5b63
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-09-02 02:08:38 -0500

    Update 649. Dota2 Senate.py

--- Leetcode 75/Queue/649. Dota2 Senate.py
+++ Leetcode 75/Queue/649. Dota2 Senate.py
@@ -1,11 +1,30 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        q = deque(senate)
-        votes = {"D":1, "R":-1}
-        count = 0
-        while abs(count) < len(q):
-            v = q.popleft()
-            count += votes[v]            
-            if count > 0 and v == "D" or count<0 and v == "R":
-                q.append(v)     
-        return 'Dire' if count>0 else  'Radiant'
+        senate = list(senate) 
+        R, D = deque(), deque()
+
+        for idx, ele in enumerate(senate):
+            if ele == "R":
+                R.append(idx)
+            else:
+                D.append(idx)
+        
+        while R and D:
+            rTurn = R.popleft()
+            dTurn = D.popleft()
+
+            if rTurn < dTurn:
+                R.append(dTurn + len(senate))
+            else:
+                D.append(rTurn + len(senate))
+        return "Radiant" if R else "Dire"
+
+"""
+Here we are using two queue to keep track of the Radiant and Dire according to order. 
+Here the usage of queue is used to improve the time complexity. Queue provides popleft() O(1) operation.
+
+Time Complexity O(n)
+- The iteration will only happend till the end of the length of the array.
+Space Complexity O(n)
+- The maximum each of these queues can grow is only till the input length. Both the queue will be a linear operation.
+"""
