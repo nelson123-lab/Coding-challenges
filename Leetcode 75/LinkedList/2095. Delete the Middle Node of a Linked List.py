284eadf7ad1a665e5016bb02259d339ab4f28c95
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-09-02 23:55:14 -0500

    Update 2095. Delete the Middle Node of a Linked List.py

--- Leetcode 75/LinkedList/2095. Delete the Middle Node of a Linked List.py
+++ Leetcode 75/LinkedList/2095. Delete the Middle Node of a Linked List.py
@@ -1,1 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next:
+            return None
 
+        # Initializing the pointers
+        slow = head
+        fast = head.next.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        slow.next = slow.next.next
+        return head
+
+"""
+Here the middle node is getting removed by the using the slow and fast technique. The fast pointer always moves twice the speed of the slow pointer.
+- This makes the slow pointer reach the middle when the fast pointer reach the end of the linked list.
+- slow.next = slow.next.next enables the removal of the middle node.
+
+Time Complexity O(n)
+- Only 1 iteration is happening through the list.
+Space Compelxity O(1)
+- No additional space is used here for the algorithm.
+"""
