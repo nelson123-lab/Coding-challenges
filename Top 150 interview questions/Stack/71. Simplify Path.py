15d65b3a67f72fdb8c1052a731706c97ff01934c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-09-03 17:05:11 -0500

    Update 71. Simplify Path.py

--- Top 150 interview questions/Stack/71. Simplify Path.py
+++ Top 150 interview questions/Stack/71. Simplify Path.py
@@ -1,21 +1,24 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        
-        comps = path.split(sep = '/')
         stack = []
-         
-        for c in comps:
-            if not c or c == "." or c == '/':
-				# ignore these 
-                continue
-            elif c == "..":
-				# if we can go back a directory
-                if stack:
-                    stack.pop()
-            else:
-				# this is a normal directory name, add to stack
-                stack.append(c)
-        # join what's left in stack with '/' characters
-        res = "/".join(c for c in stack)
-        return '/' + res
+        file = ""
+
+        for cF in path + "/":
+		
+            if cF == "/":
+                if file != "" and file != "." and file != "..": # appending to stack
+                    stack.append(file)
+                elif file == "..": # poping out of the stack if there exist a value in stack.
+                    if stack: stack.pop()
+                file = "" # Making the value of the file to empty string.
+		    
+            else: file += cF
+        return "/" + "/".join(stack)
         
+"""
+- Since we need to remove the file of the current storage if the file is "..", we are using a stack to keep track of the job location.
+Time Complexity O(n)
+- There is one iteration happening here. The time complexity of join operation is O(n) but this is outside the for loop.
+Space Complexiy O(n)
+- The space used by stack
+"""
