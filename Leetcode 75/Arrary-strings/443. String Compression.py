ac68d1e4a919ec7f3421d04a2caff193efcd10ab
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-09-06 23:58:01 -0500

    Update 443. String Compression.py

--- Leetcode 75/Arrary-strings/443. String Compression.py
+++ Leetcode 75/Arrary-strings/443. String Compression.py
@@ -1,3 +1,30 @@
+"""
+The main thing to note here is that we are not allowed to use extra space to store the compressed string as per the description so our algorithm should work in O(n) space Complexity.
+- If we could have returned the compressed string separately it is an easy problem. We can do this in the below way.
+"""
+class Solution:
+    def compress(self, chars) -> int:
+        ref = {}
+        res = ""
+        for i in range(len(chars)):
+            if chars[i] not in ref:
+                ref[chars[i]] = 1
+            else:
+                ref[chars[i]] = ref[chars[i]] + 1
+        
+        for key, value in ref.items():
+            res = res + key + str(value)
+
+        return len(res), res
+a = Solution()
+print(a.compress(["a","a","b","b","c","c","c"]))
+
+# Output 
+#(6, 'a2b2c3')
+
+"""
+The above solution is not accepted. Thus we need to edit the values of the input list within itself.
+"""
 class Solution:
     def compress(self, chars) -> int:
         length = len(chars)
@@ -25,3 +52,8 @@ class Solution:
     
 a = Solution()
 print(a.compress(["a","a","b","b","c","c","c"]))
+
+""""
+Time Compelxity O(n)
+Space Complexity O(1)
+"""
