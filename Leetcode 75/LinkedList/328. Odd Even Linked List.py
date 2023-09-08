9238e1b35df524039b1e655fa7c08bf31932b685
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-09-07 23:58:26 -0500

    Update 328. Odd Even Linked List.py

--- Leetcode 75/LinkedList/328. Odd Even Linked List.py
+++ Leetcode 75/LinkedList/328. Odd Even Linked List.py
@@ -5,22 +5,35 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
+        # Checking if there is no head, only one value or only 2 values respectively, then we can simply return the head.
+        if not head or not head.next or not head.next.next:
+            return head
 
-        odd = head
-        even = head.next
-        even_head = even
+        # Initializing odd and even pointers
+        oddP = head
+        evenP = head.next
+        even_head = evenP
 
-        while even is not None and even.next is not None:
-            odd.next = even.next
-            odd = odd.next
+        while evenP is not None and evenP.next is not None:
+            oddP.next = evenP.next
+            oddP = oddP.next
 
-            even.next = odd.next
-            even = even.next
+            evenP.next = oddP.next
+            evenP = evenP.next
 
         # Attach even list to the tail of odd list
-        odd.next = even_head
+        oddP.next = even_head
 
         return head
+
+"""
+- Here the problem is to rearrange the linkedList in a way that all the values in the odd index should group together and all the even index should be grouped and come after the odd indices.
+- [1,2,3,4,5] changes to [1,3,5,2,4] here 1, 3, 5 are in the odd index positions so they were grouped together similarly 2 and 4 in the even index positions, thus grouped together.
+- The problem requires us to do the operation in O(1) space compelxity so we won't be able to use extra space to make 2 separate linkedList for odd and even and then combine together.
+- The only thing works here is rearranging the LinkedList to obtain the desired results
+
+Time Compelxity O(n)
+Space Complexity O(1)
+
+"""
 
