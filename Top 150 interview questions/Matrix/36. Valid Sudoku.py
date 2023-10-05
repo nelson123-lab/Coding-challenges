d65a87411920148ddb425f1e67a0e5ce8b47a1d0
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-04 23:58:23 -0500

    Update 36. Valid Sudoku.py

--- Top 150 interview questions/Matrix/36. Valid Sudoku.py
+++ Top 150 interview questions/Matrix/36. Valid Sudoku.py
@@ -23,3 +23,22 @@ class Solution:
 Time Complexity O(n^2)
 Space Complexity O(n)
 """
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row, col, cell = [0]*9, [0]*9, [0]*9
+        for i, j in product(range(9), repeat= 2):
+            if board[i][j] != '.':
+                mask = 1 << int(board[i][j])-1
+                c = (i//3)*3 + j//3
+                if mask&row[i] or mask&col[j] or mask&cell[c]:
+                    return False
+                row[i] |= mask
+                col[j] |= mask
+                cell[c] |= mask
+        return True
+
+"""
+Time Complexity O(n^2)
+Space Complexity O(n^2)
+"""
