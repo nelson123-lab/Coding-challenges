93a445239303a7d2b20b972b1cc2ad63f2e8979c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-06 23:57:41 -0500

    Update 36. Valid Sudoku.py

--- Top 150 interview questions/Matrix/36. Valid Sudoku.py
+++ Top 150 interview questions/Matrix/36. Valid Sudoku.py
@@ -26,10 +26,18 @@ Space Complexity O(n)
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Creating row, column and 3x3 subgrids.
         row, col, cell = [0]*9, [0]*9, [0]*9
+        # Iterating through all the possible i, j possibilities for a sudoko.
         for i, j in product(range(9), repeat= 2):
+            # Checking if the cell is empty
             if board[i][j] != '.':
+                # Creating a mask
                 mask = 1 << int(board[i][j])-1
+                # Finding in which subgrid this particular i, j cell is present.
+                """
+                eg: i, j = (5,1)  will result in c value = 3, 3rd subgrid.
+                """
                 c = (i//3)*3 + j//3
                 if mask&row[i] or mask&col[j] or mask&cell[c]:
                     return False
@@ -37,8 +45,37 @@ class Solution:
                 col[j] |= mask
                 cell[c] |= mask
         return True
-
 """
-Time Complexity O(n^2)
-Space Complexity O(n^2)
+```
+board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+```
+
+Let's focus on the cell at row `0` and column `1`, which contains the number `3`.
+
+1. `board[0][1]` gives us the value of the cell at row `0` and column `1`, which is `"3"`.
+
+2. `int(board[0][1])` converts the string `"3"` to an integer, giving us the value `3`.
+
+3. `int(board[0][1])-1` subtracts `1` from `3`, resulting in `2`.
+
+4. `1 << int(board[0][1])-1` performs a bitwise left shift operation. The number `1` is shifted to the left by `2` positions.
+
+   In binary representation, `1` is `0001`. Shifting it to the left by `2` positions gives us `0100`, which is `4` in decimal representation.
+
+   So, the value of `mask` becomes `4`.
+
+In this example, the line of code `mask = 1 << int(board[i][j])-1` sets `mask` to `4`, representing the presence of the number `3` in the Sudoku board.
+
+Time Complexity O(n^2) but it simplifies to O(1) since the value of n is constant doesn't depend on the input.
+Space Complexity O(n) but it simpleifies to O(1) since the value of n is 9. It doesn't change according to the input. 
 """
