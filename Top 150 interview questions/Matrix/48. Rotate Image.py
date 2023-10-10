8f28405fb4e5bef91a2860c7c8d398fbf948afb5
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-09 23:53:59 -0500

    Update 48. Rotate Image.py

--- Top 150 interview questions/Matrix/48. Rotate Image.py
+++ Top 150 interview questions/Matrix/48. Rotate Image.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        temp = [list(i) for i in list(zip(*matrix))]
+        res = []
+        for i in temp:
+            res.append(i[::-1])
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] = res[i][j]
+        
