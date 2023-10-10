a3cefb75a110916235e47d779ac4d8847193406a
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-09 23:59:41 -0500

    Delete two sum.py

--- two sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            a = target - nums[i]
-            if a in nums and nums.index(a) != i:
-                return [i, nums.index(a)]
-            else:
-                pass
