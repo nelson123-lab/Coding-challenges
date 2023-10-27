9eb7a464663d32ff6e21ce64a617f8f6c7b07c5b
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-26 19:04:49 -0500

    Update 3Sum_closest.py

--- Daily Challenges/3Sum_closest.py
+++ Daily Challenges/3Sum_closest.py
@@ -1,23 +1,31 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()  # Sort the input array in ascending order.
-        closest_sum = float('inf')  # Initialize closest_sum with positive infinity.
-
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1  # Pointers for the remaining subarray.
-            
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-
-                # If the current sum is closer to the target than the previous closest sum,
-                # update closest_sum.
-                if abs(current_sum - target) < abs(closest_sum - target):
-                    closest_sum = current_sum
-
-                if current_sum < target:
-                    left += 1  # Move the left pointer to increase the sum.
+        # Sorting the numbers
+        nums.sort()
+        # Initializing distance
+        distance = float('inf')
+        # Iterating through the list
+        for idx in range(len(nums) - 2):
+            l, r = idx+1, len(nums)-1
+            new_target = target - nums[idx]
+            while l < r:
+                two_sum = nums[l] + nums[r]
+                curr_distance = new_target - two_sum
+                
+                if abs(distance) > abs(curr_distance):
+                    distance = curr_distance
+                # Checking different condition for the closest.
+                if two_sum == new_target:
+                    return target
+                elif two_sum < new_target:
+                    l += 1
                 else:
-                    right -= 1  # Move the right pointer to decrease the sum.
+                    r -= 1
+        return target - distance
 
-        return closest_sum
-        
+"""
+This is similar to 3sum problem but here we need to check with distance that is minimal each for all the different combinations of 3 numbers.
+Time Complexity O(n^2)
+- This is due to the while loop inside the for loop that works for n times within the loop in the worst case scenario.
+Space Complexity O(1)
+"""
