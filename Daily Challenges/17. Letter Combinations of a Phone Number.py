d1ac7157410db7a90289b57878faabb09e28ac48
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-10-27 23:56:57 -0500

    Update 17. Letter Combinations of a Phone Number.py

--- Daily Challenges/17. Letter Combinations of a Phone Number.py
+++ Daily Challenges/17. Letter Combinations of a Phone Number.py
@@ -25,4 +25,33 @@ class Solution:
             digit_list = [numdir[char] for char in list(digits)]
             output = self.find_combinations(digit_list)
         return output
-        
+
+"""
+Time complexity O(3^n)
+Space Completely O(3^n)
+"""
+
+# Easier solution
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits=="":
+            return []
+        d = ["", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+        n = len(digits)
+        a = []
+        def solve(i,s):
+            if len(s)==n:
+                a.append("".join(s))
+            if i==n:
+                return
+            for j in range(len(d[int(digits[i])])):
+                s.append(d[int(digits[i])][j])
+                solve(i+1,s)
+                s.pop()
+                solve(i+1,s)
+            return 
+        solve(0,[])
+        return a
+
+
