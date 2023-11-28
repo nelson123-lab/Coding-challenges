ba12190917730955dade05cc15cdd1447674aec3
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-11-27 23:59:06 -0600

    Update 2824. Count Pairs Whose Sum is Less than Target.py

--- Daily Challenges/2824. Count Pairs Whose Sum is Less than Target.py
+++ Daily Challenges/2824. Count Pairs Whose Sum is Less than Target.py
@@ -16,3 +16,23 @@ Time Complexity O(n^2)
 Space Complexity O(n)
 """
 
+"""
+Here we can even use the two pointer approach since we only need to know whether two pairs are less than the target we don't need to know what are the indices of the pairs.
+"""
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        l, r = 0, len(nums)-1
+        no_pairs = 0
+        while l < r:
+            if nums[l] + nums[r] < target:
+                no_pairs += (r - l) # This means if the above statement is true then all the values in between the r and l will be true.
+                l += 1
+            else: r -= 1
+        return no_pairs
+
+"""
+Time complexity O(nlogn)
+Space Complexity O(n)
+"""
