6d0608b31136bcd5945b0f37ffc4fd896b3a397c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-11-29 23:54:47 -0600

    Update 73. Set Matrix Zeroes.py

--- Top 150 interview questions/Matrix/73. Set Matrix Zeroes.py
+++ Top 150 interview questions/Matrix/73. Set Matrix Zeroes.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        zeros = set()
+        # setZeros = set()
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    zeros.add((i,j))
+        for x,y in zeros:
+            for k in range(len(matrix[0])):
+                matrix[x][k] = 0
+            for l in range(len(matrix)):
+                matrix[l][y] = 0
+                
