50b6f4a0d6261027e559df9784abd3410a99a845
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-12-03 23:55:05 -0600

    Create 35. Search Insert Position.py

---
+++ Practice Problems/35. Search Insert Position.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        nearest_index = 0
+        while l<r:
+            mid = l+r//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+                nearest_index = mid
+            else: 
+                l = mid + 1
+            
