fd6c864aa2bcaa50ac88144485ba9784dc0c5927
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-12-03 23:59:16 -0600

    Update 35. Search Insert Position.py

--- Practice Problems/35. Search Insert Position.py
+++ Practice Problems/35. Search Insert Position.py
@@ -3,7 +3,7 @@ class Solution:
         l, r = 0, len(nums)-1
         nearest_index = 0
         while l<r:
-            mid = l+r//2
+            mid = r-l//2
             if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
@@ -11,4 +11,10 @@ class Solution:
                 nearest_index = mid
             else: 
                 l = mid + 1
+                nearest_index = mid
+        return nearest_index
             
+            
+"""
+Here we need to do a binary search to find the target and run the algorithm in O(logn) complexity.
+"""
