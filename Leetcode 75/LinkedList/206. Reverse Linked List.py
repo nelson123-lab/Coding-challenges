ae045876d38ba56b3a69e9e605054e762989bff7
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-12-04 23:54:53 -0600

    Update 206. Reverse Linked List.py

--- Leetcode 75/LinkedList/206. Reverse Linked List.py
+++ Leetcode 75/LinkedList/206. Reverse Linked List.py
@@ -1,1 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            n = curr.next
+            curr.next = prev
+            prev = curr
+            curr = n
+        return prev
 
+"""
+The reverseList method initializes prev to None and curr to the head of the linked list. 
+Then, it iterates through the linked list, reversing the pointers to the nodes. Finally, it returns the new head of the reversed linked list.
+"""
