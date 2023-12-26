a78e288c0f460fa4b66104d110d3e89f21c25645
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-12-26 22:40:30 +0530

    Delete Introduction to Pandas/2879. Display the First Three Rows.py

--- Introduction to Pandas/2879. Display the First Three Rows.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-    return employees.head(3)
-
-"""
-The .head method helps in displaying the rows of data from a dataframe. You can choose the number of rows to be displayed.
-"""
