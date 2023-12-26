a42cb46ec370e9c3ab4c0bcc557a59c269e22f15
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-12-26 22:40:22 +0530

    Delete Introduction to Pandas/2878. Get the Size of a DataFrame.py

--- Introduction to Pandas/2878. Get the Size of a DataFrame.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def getDataframeSize(players: pd.DataFrame) -> List[int]:
-    return list(players.shape)
-
-"""
-DataFrame.shape return a tuple with the number of rows and number of columns in the dataframe.
-"""
