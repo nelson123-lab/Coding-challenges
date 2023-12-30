92bedc0cce63188a3a9050414eef900179ade6b2
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-12-30 23:43:52 +0530

    Delete 1732. Find the highest altitude.py

--- 1732. Find the highest altitude.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def largestAltitude(self, gain: List[int]) -> int:
-        alt = [0]
-        for idx, ele in enumerate(gain):
-            alt.append(alt[-1]+gain[idx])
-        return max(alt)
