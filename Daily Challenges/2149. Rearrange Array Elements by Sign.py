36f7e9fdab8393616da19a32c3efb570ff24e997
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-02-23 00:08:10 -0600

    Update 2149. Rearrange Array Elements by Sign.py

--- Daily Challenges/2149. Rearrange Array Elements by Sign.py
+++ Daily Challenges/2149. Rearrange Array Elements by Sign.py
@@ -4,22 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        output = [0] * n
-        i, pos, neg = 0, 0, 1
-
-        while i < n:
-            if nums[i] > 0:
-                output[pos] = nums[i]
-                pos += 2
+        even, odd = 0, 1
+        res = [0]* len(nums)
+        for k in range(len(nums)):
+            if nums[k] > 0:
+                res[even] = nums[k]
+                even += 2
             else:
-                output[neg] = nums[i]
-                neg += 2
-            i += 1
-
-        return output
+                res[odd] = nums[k]
+                odd += 2
+        return res
 
   """
+
+  - Here the solution uses Two pointer even and odd to keep track of the positive and negative numbers. All the positive numbers are present in the even positions according to the index(0,2,4,6) and all the negative values are
+  present in the odd positions (1,3,5,7). 
+  - We are then using a pointer k to keep track of each of the elements within the nums and adding the positive and negative numbers each time to the res list.
   Time Complexity O(n)
   Space Compelexity O(n)
   """
