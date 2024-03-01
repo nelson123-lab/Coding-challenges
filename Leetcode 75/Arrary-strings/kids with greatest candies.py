dd8dd14a379756b85aa8e22add64970d1c798354
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-01 11:56:01 -0600

    Update kids with greatest candies.py

--- Leetcode 75/Arrary-strings/kids with greatest candies.py
+++ Leetcode 75/Arrary-strings/kids with greatest candies.py
@@ -18,3 +18,40 @@ class Solution:
 
 ans = Solution()
 print(ans.kidsWithCandies(candies = [2,3,5,1,3], extraCandies = 3))
+
+
+"""
+This is a two pointer approach for solving this question
+Time complexity O(n)
+Space Complexity O(1) we have the option to return one result at a time but only because the question needs a list we are using it.
+""""
+
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        max_candy = max(candies)
+        res = []
+        for i in candies:
+            if i + extraCandies >= max_candy:
+                res.append(True)
+            else: res.append(False)
+        return res
+
+"""
+Time complexity O(n)
+Space Complexity O(1)
+"""
+
+# One Liner
+
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        return [(i + extraCandies) >= max(candies) for i in candies]
+
+"""
+This is a one liner solution with a list comprehension. Here the boolean values get automatically added to the list.
+"""
