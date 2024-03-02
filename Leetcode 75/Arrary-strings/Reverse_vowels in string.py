5c32fc3b3a7eceaadfa43ece4426ef5bdb729c87
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-02 03:29:26 -0600

    Added two more solutions for Reverse_vowels in string.py

--- Leetcode 75/Arrary-strings/Reverse_vowels in string.py
+++ Leetcode 75/Arrary-strings/Reverse_vowels in string.py
@@ -1,3 +1,4 @@
+# Solution using Two pointer approach
 class Solution:
     def reverseVowels(self, s: str) -> str:
         l, r = 0, len(s)-1
@@ -15,3 +16,61 @@ class Solution:
                 l += 1
                 r -= 1
         return "".join(s_list)
+"""
+Time Complexity O(n2)
+Space Complexity O(n)
+"""
+
+# Solution using an empty string and a different approach.
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ""
+        vowels = {"a", "A", "e", "E", "i", "I", "o", "O", "u", "U"}
+        v = []
+        for i in range(len(s)):
+            if s[i] in vowels: v.append(s[i])
+            else: pass
+
+        v = v[::-1]
+        v_c = 0
+        for i in range(len(s)):
+            if s[i] not in vowels: res += s[i]
+            else: 
+                res += v[v_c]
+                v_c += 1
+        return res
+
+"""
+Time Complexity O(n), it uses a set instead of a list so search operation within a list is O(1)
+Space Complexity O(n)
+"""
+
+# Solution with Inplace update
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = {"a", "A", "e", "E", "i", "I", "o", "O", "u", "U"}
+        v = []
+        for i in range(len(s)):
+            if s[i] in vowels:
+                v.append(s[i])
+            else: pass
+        v = v[::-1]
+        v_c = 0
+        for i in range(len(s)):
+            if s[i] in vowels:
+                s = s[:i] + v[v_c] + s[i+1:]
+                v_c += 1
+        return s
+
+"""
+Time Complexity O(n)
+Space Complexity O(n)
+"""
