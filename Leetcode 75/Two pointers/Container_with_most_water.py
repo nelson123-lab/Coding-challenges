b3e698dc88b7a86b09297143bf77ade5b3ceb6bd
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-13 05:09:57 -0500

    Update Container_with_most_water.py
    Proper explanation is added with time and space compleixty.

--- Leetcode 75/Two pointers/Container_with_most_water.py
+++ Leetcode 75/Two pointers/Container_with_most_water.py
@@ -1,17 +1,25 @@
-class Solution:
-    def maxArea(self, height) -> int:
-        left = 0
-        right = len(height) - 1
-        maxArea = 0
-        
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right, maxArea = 0, len(height) - 1, 0
+
         while left < right:
-            currentArea = min(height[left], height[right]) * (right - left)
-            maxArea = max(maxArea, currentArea)
-            
+            currArea = min(height[left], height[right]) * (right-left)
+            maxArea = max(maxArea, currArea)
+
             if height[left] < height[right]:
                 left += 1
-            else:
+            else: 
                 right -= 1
-        
+
         return maxArea
 
+"""
+- Here it uses two pointer approach to find the maxArea of water contained between the two pillars.
+- Area can be found by finding the minimum height value among the two pillars multiplied with distance.
+Time Complexity O(n)
+Space Complexity O(1)
+""""
