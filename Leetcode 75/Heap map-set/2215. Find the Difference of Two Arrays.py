0f12085bfc2bb4fc04dfabc6a5a0c7a839351f53
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-15 23:44:26 -0500

    Create 2215. Find the Difference of Two Arrays.py

--- Leetcode 75/Heap map-set/2215. Find the Difference of Two Arrays.py
+++ Leetcode 75/Heap map-set/2215. Find the Difference of Two Arrays.py
@@ -1,19 +1,17 @@
-class Solution:
+class Solution(object):
     def findDifference(self, nums1, nums2):
-        nums, nums2 = list(set(nums1)), list(set(nums2))
-      """
-      Making a copy of the list. When elements of a list are removed while iteration it will result in not iterating through all the elements.
-      """
-        a = nums.copy()
-        for i in nums:
-            if i in nums2:
-                nums2.remove(i)
-                a.remove(i)
-            else: pass
-        return [a, nums2]
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        A, B = set(nums1), set(nums2)
 
-# The same results can be obtained by using set operations
-class Solution:
-    def findDifference(self, nums1, nums2):
-        nums1, nums2 = set(nums1), set(nums2)
-        return [list(nums1-nums2), list(nums2-nums1)]
+        return [list(A-B), list(B-A)]
+        
+"""
+This is a quesiton using sets. We are converting the sets in A and B and finding the differnce using the set methods.
+Time complexity O(n)
+Space Complexity O(n)
+where n = n1 + n2
+"""
