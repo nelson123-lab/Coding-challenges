f9f169c95e5c14f2fbeae1a17c795735a93a4b53
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-22 00:31:56 -0500

    Update and rename SQL 50/620. Not Boring Movies.sql to SQL 50/BASIC AGGREGATE FUNCTIONS/620. Not Boring Movies.sql

--- SQL 50/BASIC AGGREGATE FUNCTIONS/620. Not Boring Movies.sql
+++ SQL 50/BASIC AGGREGATE FUNCTIONS/620. Not Boring Movies.sql
@@ -1,2 +1,6 @@
 SELECT id, movie, description, rating FROM Cinema WHERE id % 2 != 0 AND description != 'boring'
 ORDER BY rating DESC
+
+"""
+- Here we need to return the odd number of movies with description not equal to boring.
+"""
