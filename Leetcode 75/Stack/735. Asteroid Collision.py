afec420a8c62fe0ac0e2b9a925822eab256101bc
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-26 22:24:47 -0500

    Update 735. Asteroid Collision.py

--- Leetcode 75/Stack/735. Asteroid Collision.py
+++ Leetcode 75/Stack/735. Asteroid Collision.py
@@ -1,3 +1,4 @@
+# Solution from a youtube video.
 class Solution:
     def asteroidCollision(self, asteroids):
         s = []
@@ -11,3 +12,33 @@ class Solution:
 
 a = Solution()
 print(a.asteroidCollision([10,2,-5]))
+
+
+# More understandable version.
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        s = []
+        for a in asteroids:
+            while s and s[-1] > 0 and a < 0:
+                if s[-1]  < -a : 
+                    s.pop()
+                elif s[-1]  > -a : 
+                    break
+                else: 
+                    s.pop()
+                    break
+            else: 
+                s.append(a)
+        return s
+
+"""
+- Here the main idea is to remove any asteriod that has less power to the left when a negative power appears. 
+eg: [5, 6, -10] gonna return [] since the -10 is gonna remove both 5 and 6. That's why we are using a while loop within the for loop to check the power of the asteriod.
+
+Time COmplexity O(n) even in the worst case scenario the input list will be processed only once.
+Space Complexity O(n)
+"""
