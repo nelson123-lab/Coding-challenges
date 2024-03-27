5cc523a9ad502911be9d75c3e77238e96997e94a
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-26 22:17:30 -0500

    Update 2390. Removing Stars From a String.py

--- Leetcode 75/Stack/2390. Removing Stars From a String.py
+++ Leetcode 75/Stack/2390. Removing Stars From a String.py
@@ -12,4 +12,25 @@ class Solution:
 """
 The algorithm works by checking if the current character is a star or not. If it is not a star, it will be added to the results.
 If it is a star the last element from the res array will be removed. It is similar to removing the character close to the star.
+Time Complexity O(n)
+Space Complexity O(n)
+"""
+
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = []
+        for i in s:
+            if i == "*":
+                res.pop()
+            else:
+                res.append(i)
+        return "".join(res)
+
+"""
+Time Complexity O(n)
+Space Complexity O(n) uses n extra space in the worst case scenario.
 """
