199afc76268bcafda20e7cf0110ae8139045f921
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-05-04 13:56:30 -0500

    Update 1731. The Number of Employees Which Report to Each Employee.sql

--- SQL 50/ADVANCED SELECT AND JOINS/1731. The Number of Employees Which Report to Each Employee.sql
+++ SQL 50/ADVANCED SELECT AND JOINS/1731. The Number of Employees Which Report to Each Employee.sql
@@ -1,3 +1,5 @@
+-- Using Common table expression
+
 WITH managers AS(
     SELECT reports_to AS employee_id,
             COUNT(reports_to) AS reports_count,
@@ -15,3 +17,12 @@ FROM managers AS m
 INNER JOIN Employees AS e
 USING(employee_id)
 ORDER BY employee_id;
+
+-- Using JOIN Operations.
+
+SELECT mang.employee_id, mang.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age FROM Employees emp JOIN Employees mang
+ON emp.reports_to = mang.employee_id
+GROUP BY employee_id
+ORDER by employee_id
+
+-- Here we are using a self-join to make the number of employees who report to the manager.
