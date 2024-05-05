2bd82e36bfc466d4d8df1dfe81b44c18f98f3412
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-05-05 00:20:30 -0500

    FIxed the issue within the soluiton.

--- SQL 50/ADVANCED SELECT AND JOINS/1789. Primary Department for Each Employee.sql
+++ SQL 50/ADVANCED SELECT AND JOINS/1789. Primary Department for Each Employee.sql
@@ -1,9 +1,9 @@
--- Write your MySQL query statement below
-select employee_id, department_id
-from Employee
-Where primary_flag = 'Y'
-UNION
-select employee_id, department_id
-from Employee
-group by employee_id
-having count(employee_id) = 1
+(SELECT employee_id, department_id FROM Employee
+GROUP BY employee_id HAVING COUNT(department_id) = 1)
+UNION 
+(SELECT employee_id, department_id FROM Employee
+WHERE primary_flag = 'Y');
+
+-- Here we combine the results of 2 queries using the union function.
+-- The first part is when there is no primary department when the employee joins for the first time. This is when the no of departments the employee works in will be 1.
+-- Second part is when the employee works in multiple departments and we are selecting only the department where the primary_flag = "Y"
