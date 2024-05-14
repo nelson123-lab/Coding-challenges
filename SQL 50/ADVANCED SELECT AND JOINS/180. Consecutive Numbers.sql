b7a2104364887eb8812db3f0ba6ac518690ca753
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-05-13 23:13:33 -0500

    Update 180. Consecutive Numbers.sql

--- SQL 50/ADVANCED SELECT AND JOINS/180. Consecutive Numbers.sql
+++ SQL 50/ADVANCED SELECT AND JOINS/180. Consecutive Numbers.sql
@@ -1,10 +1,7 @@
-# Write your MySQL query statement below
-with cte as (
-    select num,
-    lead(num,1) over() num1,
-    lead(num,2) over() num2
-    from logs
+SELECT DISTINCT a.num AS ConsecutiveNums FROM Logs a
+JOIN Logs b ON a.id = b.id + 1
+JOIN Logs c ON b.id = c.id + 1
+WHERE a.num = b.num AND b.num = c.num
 
-)
-
-select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
+-- Here we are using the self join to combine the same table skipping one row and skipping two rows.
+-- We are then selecting the distinct element to find the elements that exist consecutively using the WHERE clause.
