8ac2509a6db063e3e839a7440dedc5d55c851f30
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-05-15 00:45:54 -0500

    Fixed corrections and added explanations.

--- SQL 50/SUBQUERIES/1978. Employees Whose Manager Left the Company.sql
+++ SQL 50/SUBQUERIES/1978. Employees Whose Manager Left the Company.sql
@@ -1,4 +1,8 @@
-SELECT employee_id
-FROM employees
-WHERE salary < 30000 AND manager_id NOT IN (SELECT employee_id FROM employees) 
-ORDER BY employee_id;
+SELECT employee_id FROM Employees WHERE salary < 30000 AND manager_id NOT IN 
+(
+    SELECT employee_id FROM Employees
+)
+ORDER BY employee_id
+
+-- Here we are first taking the employee_id with a salary less than 30000 and then checking if the manager_id is still present in the employee_id
+-- Returning all the employee_id with manager_id not present in the subquery.
