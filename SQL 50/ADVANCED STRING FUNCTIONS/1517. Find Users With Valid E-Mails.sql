92ba390b0de6402196aefba4c91a5d94d52e1532
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-05-19 13:32:51 -0500

    Corrected solution and added explanation

--- SQL 50/ADVANCED STRING FUNCTIONS/1517. Find Users With Valid E-Mails.sql
+++ SQL 50/ADVANCED STRING FUNCTIONS/1517. Find Users With Valid E-Mails.sql
@@ -1,3 +1,11 @@
 SELECT *
 FROM Users
-WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com';
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
+
+--  Regular Expression Pattern
+-- `^`: Asserts the start of the string.
+-- `[a-zA-Z]`: Matches any uppercase or lowercase letter. This ensures that the email address starts with a letter.
+-- `[a-zA-Z0-9_.-]*`: Matches zero or more occurrences of uppercase or lowercase letters, digits, underscores (`_`), dots (`.`), or hyphens (`-`). 
+-- `@leetcode`: Matches the literal string `@leetcode`.
+-- `\\.com`: Matches the literal string `.com`. The backslash (`\\`) is used to escape the dot. By escaping it, we specify that we want to match an actual dot.
+-- `$`: Asserts the position at the end of the string.
