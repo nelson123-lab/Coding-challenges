31917d256f814b94c383f384935f29f6e71de055
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-05-18 19:18:00 -0500

    Updated the solution with explanation.

--- SQL 50/ADVANCED STRING FUNCTIONS/1327. List the Products Ordered in a Period.sql
+++ SQL 50/ADVANCED STRING FUNCTIONS/1327. List the Products Ordered in a Period.sql
@@ -1,7 +1,10 @@
-# Write your MySQL query statement below
-SELECT 
-    p.product_name,
-    SUM(o.unit) AS unit
-FROM Products p
-INNER JOIN Orders o ON p.product_id = o.product_id AND MONTH(o.order_date) = 2 AND YEAR(order_date) = 2020
-GROUP BY o.product_id HAVING unit >= 100;
+-- Write your MySQL query statement below
+SELECT product_name, SUM(unit) AS unit FROM Orders AS O
+LEFT JOIN Products AS P
+ON P.product_id = O.product_id
+WHERE O.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY O.product_id HAVING unit >= 100
+
+-- Here we use a LEFT JOIN to combine the Products and the order table. 
+-- Extract the order date in February.
+-- Grouping by product_id and having the unit total price greater than 100.
