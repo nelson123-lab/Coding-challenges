8c56997f7b013814f93855aa09dc94c0279675be
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-05-26 23:56:14 -0500

    Create 739. Daily Temperatures.py

---
+++ Leetcode 75/Monotonic Stack/739. Daily Temperatures.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temps):
+        results = [0] * len(temps)
+        stack = []
+        for i, temp in enumerate(temps):
+            while stack and temps[stack[-1]] < temp:
+                index = stack.pop()
+                results[index] = i - index
+            stack.append(i)
+
+        return results
+"""
+- Here we use a stack to find the daily temperature increase. 
+
+Time Complexity O(n)
+Space Complexity O(n)
+"""
