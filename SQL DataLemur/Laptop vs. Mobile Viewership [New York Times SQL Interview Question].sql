3d36af85cde970c22cd6847c60da7ebf703630eb
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-06-04 22:32:02 -0500

    Added explanation and structuring.

--- SQL DataLemur/Laptop vs. Mobile Viewership [New York Times SQL Interview Question].sql
+++ SQL DataLemur/Laptop vs. Mobile Viewership [New York Times SQL Interview Question].sql
@@ -1,4 +1,10 @@
 SELECT 
-  SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views, 
-  SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_views 
-FROM viewership;
+    SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_reviews,
+    SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_reviews
+FROM 
+    viewership;
+
+/*
+- Here we are first creating the number of reviews using the CASE statement. 
+- The first CASE statement is used to extract the laptop reviews and the second CASE statement for extracting the mobile_reviews 
+*/
