2f11be5a9265de98407cb61a6c92da7837d5f447
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-06-09 15:23:24 -0500

    Added optimized solution and explanation

--- SQL DataLemur/Duplicate Job Listings [Linkedin SQL Interview Question].sql
+++ SQL DataLemur/Duplicate Job Listings [Linkedin SQL Interview Question].sql
@@ -1,11 +1,25 @@
-SELECT COUNT(DISTINCT company_id) AS duplicate_companies
-FROM (
-  SELECT 
-    company_id, 
-    title, 
-    description, 
-    COUNT(job_id) AS job_count
-  FROM job_listings
-  GROUP BY company_id, title, description
-) AS job_count_cte
-WHERE job_count > 1;
+WITH dup_com_cte AS (
+    SELECT 
+        company_id 
+    FROM 
+        job_listings
+    GROUP BY 
+        company_id, 
+        title, 
+        description 
+    HAVING 
+        COUNT(company_id) > 1
+)
+
+SELECT 
+    COUNT(company_id) AS duplicate_companies 
+FROM 
+    dup_com_cte;
+
+-- Explanation
+/*
+- Here we are grouping by company_id, title, and description to find the company_id with the same job listings posted with the same job description.
+- Then counting the number of the company_id to find the no of duplicate companies.
+*/
+
+
