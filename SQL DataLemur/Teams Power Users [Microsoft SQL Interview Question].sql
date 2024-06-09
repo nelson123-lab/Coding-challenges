26b277c62d848c6f9aa9c8cea0eef1b4a0a4d58c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-06-09 14:12:31 -0500

    Added Explanation

--- SQL DataLemur/Teams Power Users [Microsoft SQL Interview Question].sql
+++ SQL DataLemur/Teams Power Users [Microsoft SQL Interview Question].sql
@@ -1,11 +1,12 @@
--- Solution using COUNT to count the number of messages
--- Using a WHERE condition to check the month and year and grouping my the sender_id and the count messages.
-SELECT 
-  sender_id,
-  COUNT(message_id) AS count_messages
+SELECT sender_id, COUNT(message_id) AS counts
 FROM messages
-WHERE EXTRACT(MONTH FROM sent_date) = '8'
+WHERE EXTRACT(MONTH FROM sent_date) = '8' 
   AND EXTRACT(YEAR FROM sent_date) = '2022'
 GROUP BY sender_id
-ORDER BY count_messages DESC
-LIMIT 2; 
+ORDER BY counts DESC
+LIMIT 2;
+
+-- Explanation
+-- Solution using COUNT to count the number of messages
+-- Using a WHERE condition to check the month and year and grouping the sender_id and the count messages.
+-- Using LIMIT 2 to display the first 2 sender_id based on the count of messages
