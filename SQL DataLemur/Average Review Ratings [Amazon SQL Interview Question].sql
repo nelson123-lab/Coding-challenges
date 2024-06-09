7c9c12068a887c8a0c8cbc1feed4d4c5efc2f237
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-06-09 17:35:52 -0500

    Added explanation and optimized query

--- SQL DataLemur/Average Review Ratings [Amazon SQL Interview Question].sql
+++ SQL DataLemur/Average Review Ratings [Amazon SQL Interview Question].sql
@@ -1,9 +1,17 @@
 SELECT 
-  EXTRACT(MONTH FROM submit_date) AS mth,
-  product_id,
-  ROUND(AVG(stars), 2) AS avg_stars
-FROM reviews
+    EXTRACT(MONTH FROM submit_date) AS mth,
+    product_id, 
+    ROUND(AVG(stars), 2) AS avg_stars
+FROM 
+    reviews
 GROUP BY 
-  EXTRACT(MONTH FROM submit_date), 
-  product_id
-ORDER BY mth, product_id;
+    mth, 
+    product_id
+ORDER BY 
+    mth, product_id;
+
+-- Explanation
+/*
+- Here we are first extracting the month and then grouping by month and product_Id to get the products grouped per month.
+- Finding the average star ratings per month using the aggregate AVG() function of GROUP BY ()
+*/
