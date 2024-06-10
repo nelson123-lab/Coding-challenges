5e98a53a3b4633f105276e2ca04333e66c6425a9
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-06-10 13:00:11 -0500

    Removed issues with the file formatting

--- Leetcode 75/Heap_Priority Queue/215. Kth Largest Element in an Array.py
+++ Leetcode 75/Heap_Priority Queue/215. Kth Largest Element in an Array.py
@@ -4,7 +4,7 @@ class Solution:
         n = nums[:k] # populate heap with first k elements
         heapq.heapify(n)
 		
-        # loop through rest of array and pushpop largest elements
+        # loop through the rest of array and push the largest elements
         for num in nums[k:]:
             if num > n[0]:
                 heapq.heappushpop(n, num)
