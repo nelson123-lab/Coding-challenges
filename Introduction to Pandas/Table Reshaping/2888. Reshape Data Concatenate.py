d61fb8f1f2442e395a5b9e81c26bd34a536393f9
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-06-10 12:48:25 -0500

    Renamed file name to support windows format

--- Introduction to Pandas/Table Reshaping/2888. Reshape Data Concatenate.py
+++ Introduction to Pandas/Table Reshaping/2888. Reshape Data Concatenate.py
@@ -5,6 +5,6 @@ def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return df
 
 """
-Here we are concatinating two dataframes vertically. If we wish to concat the datframes horizontally. It's given below.
+Here we are concatenating two data frames vertically. Suppose we wish to concatenate the data frames horizontally. It's given below.
 df = pd.concat([df1, df2], axis = 1)
 """
