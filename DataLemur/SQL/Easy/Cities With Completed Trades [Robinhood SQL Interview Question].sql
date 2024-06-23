75d4fa5297f21c60df20fc242592481200d26586
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-06-22 23:39:50 -0500

    Added new solution with explanation

--- DataLemur/SQL/Easy/Cities With Completed Trades [Robinhood SQL Interview Question].sql
+++ DataLemur/SQL/Easy/Cities With Completed Trades [Robinhood SQL Interview Question].sql
@@ -1,10 +1,12 @@
-SELECT 
-  users.city, 
-  COUNT(trades.order_id) AS total_orders 
-FROM trades 
-INNER JOIN users 
-  ON trades.user_id = users.user_id 
-WHERE trades.status = 'Completed' 
-GROUP BY users.city 
-ORDER BY total_orders DESC
-LIMIT 3;
+SELECT city, COUNT(*)  FROM trades AS T LEFT JOIN users AS U 
+ON T.user_id = U.user_id
+WHERE T.status = 'Completed'
+GROUP BY city
+ORDER BY count DESC
+LIMIT 3
+
+/*
+- Here we need to first do a left join to get the city in which trade is happening for each of the users.
+- We are filtering out the completed orders 
+- A group by city is required to find the count of the trades
+/*
