3b44fdb452bc3839bfabc46d549cb2b7053b5f80
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-07-04 23:49:11 -0500

    Added solution with explanation

--- Leetcode 75/Dynamic Programming/1137. N-th Tribonacci Number.py
+++ Leetcode 75/Dynamic Programming/1137. N-th Tribonacci Number.py
@@ -1,8 +1,20 @@
-class Solution:
-    def tribonacci(self, n: int) -> int:
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Initializing the first 3 values of the Tibonacci numbers
+        t = [0, 1, 1]
 
-        t0,t1,t2 = 0,1,1
-        for i in range(n):
-            t0,t1,t2 = t1,t2,(t0+t1+t2)
+        if n < 3:
+            return t[n]
+        for i in range(3, n + 1):
+            t[0], t[1], t[2] = t[1], t[2], sum(t)
+        return t[2]
 
-        return t0
+"""
+- For n > 3 we are using the tuple unpacking and the simultaneous assignment in Python to assign the values accordingly.
+Time Complexity O(n)
+Space Complexity O(1) since only a constant space of O(3) is used here.
+"""
