da0fc74bdaaddbf99647538a1cbda135c38f820c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-07-17 23:49:24 -0500

    Added addition solution with Explanation

--- Blind 75/Arrary/217. Contains Duplicate.py
+++ Blind 75/Arrary/217. Contains Duplicate.py
@@ -3,6 +3,24 @@ class Solution:
         return False if len(set(nums)) == len(nums) else True
 
 """
-- The logic is when the no of occurances of a particular element is more then the length will be greater than a set(array).
+- The logic is when the no of occurrences of a particular element is more then the length will be greater than a set(array).
 - Set does not contain duplicate elements.
+
+Time Complexity O(n)
+Space Compelxity O(n)
+"""
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:        
+        ref = set()
+        for ele in nums: 
+            if ele not in ref:
+                ref.add(ele)
+            else:
+                return True
+        return False
+"""
+Here we are keeping a set to keep track of the element
+Time Complexity O(n)
+Space Complexity O(n)
 """
