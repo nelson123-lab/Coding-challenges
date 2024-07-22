8b059e469099aa200dbe4964c2a099c017ec1c71
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-07-21 22:39:33 -0500

    Added solution with explanation

--- Leetcode 75/DP_Multidimensional/1143. Longest Common Subsequence.py
+++ Leetcode 75/DP_Multidimensional/1143. Longest Common Subsequence.py
@@ -1,20 +1,21 @@
-class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+
         m, n = len(text1), len(text2)
-        l = []
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
 
-        for i in range(m+1):
-            v = [0]*(n+1)
-            l.append(v)
-
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-
-                if text1[i-1] == text2[j-1]:
-                    l[i][j] = l[i-1][j-1] + 1
-
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
-                    l[i][j] = max(l[i-1][j], l[i][j-1])
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-        return l[m][n]
+        return dp[m][n]
+
+"""
+Here we are creating a multidimensional array with the shape of m and n which are the length of text 1 and text 2.
+Looks for elements diagonally and if they matches then it's gets updated by one. 
+Time Complexity O(m*n)
+Space Complexity O(m*n)
+"""
