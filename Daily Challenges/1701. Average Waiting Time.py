c963583d3b1e3235a3240d2842ceaaeedd01b27c
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-07-24 23:59:11 -0500

    Update 1701. Average Waiting Time.py

--- Daily Challenges/1701. Average Waiting Time.py
+++ Daily Challenges/1701. Average Waiting Time.py
@@ -1,13 +1,21 @@
-class Solution:
-    def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        n = len(customers)
-        curTime = 0
-        totalTime = 0
-        for i in range(n):
-            if curTime <= customers[i][0]:
-                totalTime += customers[i][1]
-                curTime = customers[i][0] + customers[i][1]
+class Solution(object):
+    def averageWaitingTime(self, customers):
+        """
+        :type customers: List[List[int]]
+        :rtype: float
+        """
+        t, total = 0, 0
+
+        for arrival, order in customers:
+            if t > arrival:
+                total += t - arrival
             else:
-                totalTime += (curTime - customers[i][0] + customers[i][1])
-                curTime += customers[i][1]
-        return totalTime / n
+                t = arrival
+            total += order
+            t += order
+        return total/float(len(customers))
+
+
+"""
+- 
+"""
