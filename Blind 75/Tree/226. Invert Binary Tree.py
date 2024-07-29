5eb5e0210f5c030fff8ba7a8ab997a6bc47cc264
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-07-28 23:25:50 -0500

    Added different solution with exp

--- Blind 75/Tree/226. Invert Binary Tree.py
+++ Blind 75/Tree/226. Invert Binary Tree.py
@@ -1,9 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root: #Base Case
-            return root
-        self.invertTree(root.left) #Call the left substree
-        self.invertTree(root.right)  #Call the right substree
-        # Swap the nodes
+        # Checking if the root node exist.
+        if not root:
+            return None
+        # Swapping the values of the child nodes each time.
         root.left, root.right = root.right, root.left
-        return root # Return the root
+        # Doing recursive call.
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+"""
+- Here when we visit each node we are swapping the values of its children. ie, left and right will be swapped.
+- We are using a DFS it doesn't matter if it's pre or post-order.
+"""
